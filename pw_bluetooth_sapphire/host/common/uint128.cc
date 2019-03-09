fd1d219d400f2792e36c2e7ac5887a0cdab26f03
Arman Uguray <armansito@google.com>
2019-03-09 03:03:58 -0800

    [bt] Move host subsystem to src/
    * Moved garnet/drivers/bluetooth/bt-host -> src/connectivity/bluetooth/core/bt-host
    * Moved garnet/drivers/bluetooth/lib/* -> src/connectivity/bluetooth/core/bt-host
    * Moved bt-host-unittests build target definition to bt-host/BUILD.gn
    
    All host subsystem code (bt-host driver and protocol libraries) now
    lives under the same bt-host directory.
    
    Test: Run bt-host-unittests and bt-host-integration-tests
    
    Change-Id: I57090b72175591f2ae891ab2725efc80ad4b8e4e

---
+++ pw_bluetooth_sapphire/host/common/uint128.cc
@@ -0,0 +1,15 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "uint128.h"
+
+#include "random.h"
+
+namespace btlib {
+namespace common {
+
+UInt128 RandomUInt128() { return Random<UInt128>(); }
+
+}  // namespace common
+}  // namespace btlib
---
+++ pw_bluetooth_sapphire/host/gatt/fake_layer.cc
@@ -0,0 +1,66 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fake_layer.h"
+
+namespace btlib {
+namespace gatt {
+namespace testing {
+
+void FakeLayer::Initialize() {
+  // TODO: implement
+}
+
+void FakeLayer::ShutDown() {
+  // TODO: implement
+}
+
+void FakeLayer::AddConnection(DeviceId peer_id,
+                              fbl::RefPtr<l2cap::Channel> att_chan) {
+  // TODO: implement
+}
+
+void FakeLayer::RemoveConnection(DeviceId peer_id) {
+  // TODO: implement
+}
+
+void FakeLayer::RegisterService(ServicePtr service, ServiceIdCallback callback,
+                                ReadHandler read_handler,
+                                WriteHandler write_handler,
+                                ClientConfigCallback ccc_callback) {
+  // TODO: implement
+}
+
+void FakeLayer::UnregisterService(IdType service_id) {
+  // TODO: implement
+}
+
+void FakeLayer::SendNotification(IdType service_id, IdType chrc_id,
+                                 DeviceId peer_id, ::std::vector<uint8_t> value,
+                                 bool indicate) {
+  // TODO: implement
+}
+
+void FakeLayer::DiscoverServices(DeviceId peer_id) {
+  // TODO: implement
+}
+
+void FakeLayer::RegisterRemoteServiceWatcher(RemoteServiceWatcher callback,
+                                             async_dispatcher_t* dispatcher) {
+  // TODO: implement
+}
+
+void FakeLayer::ListServices(DeviceId peer_id, std::vector<common::UUID> uuids,
+                             ServiceListCallback callback) {
+  // TODO: implement
+}
+
+void FakeLayer::FindService(DeviceId peer_id, IdType service_id,
+                            RemoteServiceCallback callback) {
+  // TODO: implement
+}
+
+}  // namespace testing
+}  // namespace gatt
+}  // namespace btlib
