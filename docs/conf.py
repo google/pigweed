0efdb117e11b12ee858a7d748aaa36ef2a8fc5c5
Alexei Frolov <frolv@google.com>
2019-11-14 17:22:08 -0800

    Add root docs directory
    This change adds a docs directory at the root level of the Pigweed
    repository for building documentation for the entirety of Pigweed.
    The directory contains a top-level index.rst and conf.py for docgen,
    and a pw_doc_gen build target to render the documentation.
    
    Change-Id: Iba981c64662d48a1d23bb7bda9bf0ed5a104873b

---
+++ docs/conf.py
@@ -0,0 +1,72 @@
+import sys, os
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'Pigweed'
+copyright = u'2019 Google LLC'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '0.1'
+# The full version, including alpha/beta/rc tags.
+release = '0.1.0'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ['_themes',]
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = 'Pigweed'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+html_use_smartypants = True
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'Pigweeddoc'
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'pigweed', u'Pigweed', [u'Google'], 1)
+]
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  ('index', 'Pigweed', u'Pigweed', u'Google', 'Pigweed', 'Firmware framework',
+   'Miscellaneous'),
+]
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+=======
+Pigweed
+=======
+
+Pigweed is a collection of embedded-focused libraries, which we call "modules".
+These modules are designed for small-footprint MMU-less microcontrollers like
+the ST Micro STM32L452 or the Nordic NRF82832. The modules are designed to
+facilitate easy integration into existing codebases.
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   pw_bloat/bloat
+   pw_preprocessor/docs
