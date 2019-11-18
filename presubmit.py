0a57954b8f075ab30090f0ae319677a3196685b7
Rob Mohr <mohrr@google.com>
2019-11-18 11:15:06 -0800

    Force use of CIPD packages in presubmit.py.
    Change-Id: I778515463d1f899b24591e5e779072ca3a675305

--- presubmit.py
+++ presubmit.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 
+BUILDTOOLS_GIT = 'https://pigweed.googlesource.com/infra/buildtools'
 
-def call(*cmd):
+
+def call(*args, **kwargs):
     print()
-    print('$', *cmd)
-    subprocess.check_call(cmd)
+    for k, v in sorted(kwargs.items()):
+        print('#', k, '=', v)
+    print('$', *args)
+    subprocess.check_call(args=args, **kwargs)
+
+
+def init():
+    buildtools = '.presubmit/buildtools'
+    if os.path.isdir(buildtools):
+        call('git', 'fetch', BUILDTOOLS_GIT, 'master', cwd=buildtools)
+        call('git', 'reset', '--hard', 'FETCH_HEAD', cwd=buildtools)
+    else:
+        call('git', 'clone', BUILDTOOLS_GIT, buildtools)
+
+    call(os.path.join(buildtools, 'update.py'))
+    os.environ['PATH'] = os.pathsep.join((
+        os.path.join(buildtools, 'tools'),
+        os.path.join(buildtools, 'tools', 'bin'),
+        os.environ['PATH'],
+    ))
+    print('PATH', os.environ['PATH'])
 
 
 def gn_test():
@@ -24,5 +46,6 @@ def bazel_test():
 
 
 if __name__ == '__main__':
+    init()
     bazel_test()
     gn_test()
