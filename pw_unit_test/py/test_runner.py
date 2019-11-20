8403f0a2db8c51511fd638b037920aa7bdc6390d
Alexei Frolov <frolv@google.com>
2019-11-20 13:57:54 -0800

    Update test runner to run tests from metadata
    This change adds functionality to the test runner script to search a
    directory tree for generated test metadata files, build up a graph of
    test groups, and run a user-specified list of groups with all of their
    dependencies.
    
    Tests are run through an external runner executable provided on the
    command line. The executable is called with the path to a unit test
    binary file.
    
    The old test runner functionality to run a single test directly from its
    binary path is maintained, but now uses a runner executable. This is
    configured in GN using the build variable "pw_automatic_test_runner",
    which replaces the functionality of "pw_unit_test_create_run_targets".
    
    Change-Id: Ic1ed959d87a71266408e1f26e0ae1bf906eebfb0

--- pw_unit_test/py/test_runner.py
+++ pw_unit_test/py/test_runner.py
@@ -12,25 +12,285 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-"""Script which runs Pigweed unit tests built using GN.
-
-Currently, only a single test can be run at a time. The build path and GN target
-name of the test are given to the script.
-"""
+"""Runs Pigweed unit tests built using GN."""
 
 import argparse
-import pathlib
+import enum
+import json
+import logging
 import os
+import shlex
 import subprocess
 import sys
+
+from typing import Dict, Iterable, List, Sequence, Set, Tuple
+
+import coloredlogs
+
+# Global logger for the script.
+_LOG: logging.Logger = logging.getLogger('pw_test_runner')
 
 
 def parse_args() -> argparse.Namespace:
     """Parses command-line arguments."""
 
-    parser = argparse.ArgumentParser('Run Pigweed unit tests')
-    parser.add_argument('test', type=str, help='Path to unit test binary')
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--root', type=str, default='out',
+                        help='Path to the root build directory')
+    parser.add_argument('-r', '--runner', type=str, required=True,
+                        help='Executable which runs a test on the target')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Output additional logs as the script runs')
+    parser.add_argument('runner_args', nargs=argparse.REMAINDER,
+                        help='Arguments to forward to the test runner')
+
+    # The runner script can either run binaries directly or groups.
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-g', '--group', action='append',
+                       help='Test groups to run')
+    group.add_argument('-t', '--test', action='append',
+                       help='Test binaries to run')
+
     return parser.parse_args()
+
+
+class TestResult(enum.Enum):
+    """Result of a single unit test run."""
+    UNKNOWN = 0
+    SUCCESS = 1
+    FAILURE = 2
+
+
+class Test:
+    """A unit test executable."""
+
+    def __init__(self, name: str, file_path: str):
+        self.name: str = name
+        self.file_path: str = file_path
+        self.status: TestResult = TestResult.UNKNOWN
+
+    def __repr__(self) -> str:
+        return f'Test({self.name})'
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Test):
+            return NotImplemented
+        return self.file_path == other.file_path
+
+    def __hash__(self) -> int:
+        return hash(self.file_path)
+
+
+class TestGroup:
+    """Graph node representing a group of unit tests."""
+
+    def __init__(self, name: str, tests: Iterable[Test]):
+        self._name: str = name
+        self._deps: Iterable['TestGroup'] = []
+        self._tests: Iterable[Test] = tests
+
+    def set_deps(self, deps: Iterable['TestGroup']) -> None:
+        """Updates the dependency list of this group."""
+        self._deps = deps
+
+    def all_test_dependencies(self) -> List[Test]:
+        """Returns a list of all tests in this group and its dependencies."""
+        return list(self._all_test_dependencies(set()))
+
+    def _all_test_dependencies(self, processed_groups: Set[str]) -> Set[Test]:
+        if self._name in processed_groups:
+            return set()
+
+        tests: Set[Test] = set()
+        for dep in self._deps:
+            tests.update(dep._all_test_dependencies(processed_groups))
+
+        for test in self._tests:
+            tests.add(test)
+
+        processed_groups.add(self._name)
+        return tests
+
+    def __repr__(self) -> str:
+        return f'TestGroup({self._name})'
+
+
+class TestRunner:
+    """Runs unit tests by calling out to a runner script."""
+
+    def __init__(self,
+                 executable: str,
+                 args: Sequence[str],
+                 tests: Iterable[Test]):
+        self._executable: str = executable
+        self._args: Sequence[str] = args
+        self._tests: List[Test] = list(tests)
+
+    def run_tests(self) -> None:
+        """Runs all registered unit tests through the runner script."""
+
+        for test in self._tests:
+            command = [self._executable, test.file_path, *self._args]
+            try:
+                status = subprocess.call(command)
+                if status == 0:
+                    test.status = TestResult.SUCCESS
+                else:
+                    test.status = TestResult.FAILURE
+            except subprocess.CalledProcessError as err:
+                _LOG.error(err)
+                return
+
+    def all_passed(self) -> bool:
+        """Returns true if all unit tests passed."""
+        return all(test.status is TestResult.SUCCESS for test in self._tests)
+
+
+# Filename extension for unit test metadata files.
+METADATA_EXTENSION = '.testinfo.json'
+
+
+def find_test_metadata(root: str) -> List[str]:
+    """Locates all test metadata files located within a directory tree."""
+
+    metadata: List[str] = []
+    for path, _, files in os.walk(root):
+        for filename in files:
+            if not filename.endswith(METADATA_EXTENSION):
+                continue
+
+            full_path = os.path.join(path, filename)
+            _LOG.debug('Found group metadata at %s', full_path)
+            metadata.append(full_path)
+
+    return metadata
+
+
+# TODO(frolv): This is copied from the Python runner script.
+# It should be extracted into a library and imported instead.
+def find_binary(target: str) -> str:
+    """Tries to find a binary for a gn build target.
+
+    Args:
+        target: Relative filesystem path to the target's output directory and
+            target name, separated by a colon.
+
+    Returns:
+        Full path to the target's binary.
+
+    Raises:
+        FileNotFoundError: No binary found for target.
+    """
+
+    target_path, target_name = target.split(':')
+
+    for extension in ['', '.elf', '.exe']:
+        potential_filename = f'{target_path}/{target_name}{extension}'
+        if os.path.isfile(potential_filename):
+            return potential_filename
+
+    raise FileNotFoundError(
+        f'Could not find output binary for build target {target}')
+
+
+def parse_metadata(metadata: List[str], root: str) -> Dict[str, TestGroup]:
+    """Builds a graph of test group objects from metadata.
+
+    Args:
+        metadata: List of paths to JSON test metadata files.
+        root: Root output directory of the build.
+
+    Returns:
+        Map of group name to TestGroup object. All TestGroup objects are fully
+        populated with the paths to their unit tests and references to their
+        dependencies.
+    """
+
+    def canonicalize(path: str) -> str:
+        """Removes a trailing slash from a GN target's directory.
+
+        '//module:target'  -> '//module:target'
+        '//module/:target' -> '//module:target'
+        """
+        index = path.find(':')
+        if index == -1 or path[index - 1] != '/':
+            return path
+        return path[:index - 1] + path[index:]
+
+    group_deps: List[Tuple[str, List[str]]] = []
+    all_tests: Dict[str, Test] = {}
+    test_groups: Dict[str, TestGroup] = {}
+    num_tests = 0
+
+    for path in metadata:
+        with open(path, 'r') as metadata_file:
+            metadata_list = json.load(metadata_file)
+
+        deps: List[str] = []
+        tests: List[Test] = []
+
+        for entry in metadata_list:
+            if entry['type'] == 'self':
+                group_name = canonicalize(entry['name'])
+            elif entry['type'] == 'dep':
+                deps.append(canonicalize(entry['group']))
+            elif entry['type'] == 'test':
+                test_directory = os.path.join(root, entry['test_directory'])
+                test_binary = find_binary(
+                    f'{test_directory}:{entry["test_name"]}')
+
+                if test_binary not in all_tests:
+                    all_tests[test_binary] = Test(
+                        entry['test_name'], test_binary)
+
+                tests.append(all_tests[test_binary])
+
+        if deps:
+            group_deps.append((group_name, deps))
+
+        num_tests += len(tests)
+        test_groups[group_name] = TestGroup(group_name, tests)
+
+    for name, deps in group_deps:
+        test_groups[name].set_deps([test_groups[dep] for dep in deps])
+
+    _LOG.info('Found %d test groups (%d tests).', len(metadata), num_tests)
+    return test_groups
+
+
+def tests_from_groups(args: argparse.Namespace) -> List[Test]:
+    """Returns unit tests belonging to test groups and their dependencies.
+
+    If args.names is nonempty, only searches groups specified there.
+    Otherwise, finds tests from all known test groups.
+    """
+
+    _LOG.info('Scanning for tests...')
+    metadata = find_test_metadata(args.root)
+    test_groups = parse_metadata(metadata, args.root)
+
+    groups_to_run = args.group if args.group else test_groups.keys()
+    tests_to_run: Set[Test] = set()
+
+    for name in groups_to_run:
+        try:
+            tests_to_run.update(test_groups[name].all_test_dependencies())
+        except KeyError:
+            _LOG.error('Unknown test group: %s', name)
+            sys.exit(1)
+
+    _LOG.info('Running test groups %s', ', '.join(groups_to_run))
+    return list(tests_to_run)
+
+
+def tests_from_paths(paths: List[str]) -> List[Test]:
+    """Returns a list of tests from test executable paths."""
+
+    tests: List[Test] = []
+    for path in paths:
+        name = os.path.splitext(os.path.basename(path))[0]
+        tests.append(Test(name, path))
+    return tests
 
 
 def main() -> int:
@@ -38,13 +298,42 @@ def main() -> int:
 
     args = parse_args()
 
-    try:
-        exit_status = subprocess.call([args.test])
-    except subprocess.CalledProcessError as err:
-        print(f'{sys.argv[0]}: {err}', file=sys.stderr)
-        return 1
+    log_level = 'DEBUG' if args.verbose else 'INFO'
+    coloredlogs.install(level=log_level,
+                        level_styles={'debug': {'color': 244},
+                                      'error': {'color': 'red'}},
+                        fmt='%(asctime)s | %(message)s')
 
-    return exit_status
+    runner_args = args.runner_args
+
+    if runner_args:
+        if runner_args[0] != '--':
+            _LOG.error('%s: Unrecognized argument: %s',
+                       sys.argv[0], runner_args[0])
+            _LOG.info('')
+            _LOG.info('Did you mean to pass this argument to the runner?')
+            _LOG.info('Insert a -- in front of it to forward it through:')
+            _LOG.info('')
+
+            index = sys.argv.index(runner_args[0])
+            fixed_cmd = [*sys.argv[:index], '--', *sys.argv[index:]]
+
+            _LOG.info('  %s', ' '.join(shlex.quote(arg) for arg in fixed_cmd))
+            _LOG.info('')
+
+            return 1
+
+        runner_args = runner_args[1:]
+
+    if args.test:
+        tests = tests_from_paths(args.test)
+    else:
+        tests = tests_from_groups(args)
+
+    runner = TestRunner(args.runner, runner_args, tests)
+    runner.run_tests()
+
+    return 0 if runner.all_passed() else 1
 
 
 if __name__ == '__main__':
