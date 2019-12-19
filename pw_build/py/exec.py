d74ae4820c31d08f8db7ff668a5e58d7e85fe59c
Alexei Frolov <frolv@google.com>
2019-12-19 15:44:15 -0800

    Support environment variables in pw_exec template
    This change adds support for defining environment variables in processes
    run through pw_exec. Environment variables can either be specified
    directly in the GN template or through a file (which could be generated
    from build metadata).
    
    Change-Id: Ic941b3f907143a648707a44de01da8332021270e

--- pw_build/py/exec.py
+++ pw_build/py/exec.py
@@ -13,8 +13,79 @@
 # the License.
 """Python wrapper that runs a program. For use in GN."""
 
+import argparse
+import os
+import re
 import subprocess
 import sys
+from typing import Dict, Optional
+
+
+def argument_parser(
+    parser: Optional[argparse.ArgumentParser] = None
+) -> argparse.ArgumentParser:
+    """Registers the script's arguments on an argument parser."""
+
+    if parser is None:
+        parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('-e',
+                        '--env',
+                        action='append',
+                        default=[],
+                        help='key=value environment pair for the process')
+    parser.add_argument(
+        '--env-file',
+        type=argparse.FileType('r'),
+        help='File defining environment variables for the process')
+    parser.add_argument('command',
+                        nargs=argparse.REMAINDER,
+                        help='Program to run with arguments')
+
+    return parser
+
+
+_ENV_REGEX = re.compile(r'(\w+)(\+)?=(\w+)')
+
+
+def apply_env_var(string: str, env: Dict[str, str]) -> None:
+    """Update an environment map with provided a key-value pair.
+
+    Pairs are accepted in two forms:
+
+      KEY=value    sets environment variable "KEY" to "value"
+      KEY+=value   appends OS-specific PATH separator and "value" to
+                   environment variable "KEY"
+    """
+    result = _ENV_REGEX.search(string.strip())
+    if not result:
+        return
+
+    key, append, val = result.groups()
+    if append is not None:
+        curr = env.get(key)
+        val = f'{curr}{os.path.pathsep}{val}' if curr else val
+
+    env[key] = val
+
+
+def main() -> int:
+    args = argument_parser().parse_args()
+    if not args.command or args.command[0] != '--':
+        return 1
+
+    env = os.environ.copy()
+
+    if args.env_file is not None:
+        for line in args.env_file:
+            apply_env_var(line, env)
+
+    # Apply command-line overrides at a higher priority than the env file.
+    for string in args.env:
+        apply_env_var(string, env)
+
+    return subprocess.call(args.command[1:], env=env)
+
 
 if __name__ == '__main__':
-    sys.exit(subprocess.call(sys.argv[1:]))
+    sys.exit(main())
