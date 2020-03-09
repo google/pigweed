0246148eb5095a0c05f0ab4deee72e1866f98994
Alexei Frolov <frolv@google.com>
2020-03-09 10:19:49 -0700

    pw_env_setup: Fix MacOS color escape codes
    This change switches to using "\033" instead of "\e" for ANSI escape
    codes, as "\e" doesn't seem to work on MacOS.
    
    Bug: 150
    Change-Id: Idd16001858475f261a4c0314c6a32c66d3c43ba3

--- pw_env_setup/env_setup.sh
+++ pw_env_setup/env_setup.sh
@@ -19,23 +19,19 @@ _pw_abspath () {
 }
 
 _pw_red() {
-  # echo -e "\e[0;31m$*\e[0m"
-  echo -e "$*"
+  echo -e "\033[0;31m$*\033[0m"
 }
 
 _pw_bold_red() {
-  # echo -e "\e[1;31m$*\e[0m"
-  echo -e "$*"
+  echo -e "\033[1;31m$*\033[0m"
 }
 
 _pw_green() {
-  #echo -e "\e[0;32m$*\e[0m"
-  echo -e "$*"
+  echo -e "\033[0;32m$*\033[0m"
 }
 
 _pw_bright_magenta() {
-  # echo -e "\e[0;35m$*\e[0m"
-  echo -e "$*"
+  echo -e "\033[0;35m$*\033[0m"
 }
 
 _PIGWEED_BANNER=$(cat <<EOF
