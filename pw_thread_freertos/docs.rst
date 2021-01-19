749342b879423e0d8290d7295b469f889e97d2ce
Ewout van Bekkum <ewout@google.com>
2021-01-19 14:53:19 -0800

    pw_thread_freertos: add FreeRTOS backends pw::this_thread
    Adds id, yield, and sleep backends for pw::this_thread for use on
    FreeRTOS.
    
    Change-Id: I374f671880f060d195868fd05e94b0e592b0faad
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/30521
    Commit-Queue: Ewout van Bekkum <ewout@google.com>
    Pigweed-Auto-Submit: Ewout van Bekkum <ewout@google.com>
    Reviewed-by: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Keir Mierle <keir@google.com>

---
+++ pw_thread_freertos/docs.rst
@@ -0,0 +1,8 @@
+.. _module-pw_thread_freertos:
+
+------------------
+pw_thread_freertos
+------------------
+This is a set of backends for pw_thread based on FreeRTOS. It is not ready for
+use, and is under construction.
+
---
+++ pw_thread_freertos/sleep.cc
@@ -0,0 +1,43 @@
+// Copyright 2020 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#include "pw_thread/sleep.h"
+
+#include <algorithm>
+
+#include "FreeRTOS.h"
+#include "pw_assert/assert.h"
+#include "pw_chrono/system_clock.h"
+#include "pw_chrono_freertos/system_clock_constants.h"
+#include "pw_thread/id.h"
+#include "task.h"
+
+using pw::chrono::freertos::kMaxTimeout;
+
+namespace pw::this_thread {
+
+void sleep_for(chrono::SystemClock::duration for_at_least) {
+  PW_DCHECK(get_id() != thread::Id());
+
+  // Clamp negative durations to be 0 which maps to non-blocking.
+  for_at_least = std::max(for_at_least, chrono::SystemClock::duration::zero());
+
+  while (for_at_least > kMaxTimeout) {
+    vTaskDelay(kMaxTimeout.count());
+    for_at_least -= kMaxTimeout;
+  }
+  vTaskDelay(for_at_least.count());
+}
+
+}  // namespace pw::this_thread
