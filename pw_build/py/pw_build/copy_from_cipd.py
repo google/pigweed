26241bcdfe7b33c753b02fb36ab97ec495db8c01
Armando Montanez <amontanez@google.com>
2021-03-03 18:36:25 -0800

    pw_build: GN template for copy_from_cipd
    Adds a GN template for copy_from_cipd, and updates documentation to
    provide examples on how to use.
    
    Bug: 334
    Change-Id: I0d8ab7bab4b3638ef6177a50739962d43005f47d
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/35401
    Pigweed-Auto-Submit: Armando Montanez <amontanez@google.com>
    Commit-Queue: Armando Montanez <amontanez@google.com>
    Reviewed-by: Alexei Frolov <frolv@google.com>
    Reviewed-by: Rob Mohr <mohrr@google.com>

--- pw_build/py/pw_build/copy_from_cipd.py
+++ pw_build/py/pw_build/copy_from_cipd.py
@@ -11,7 +11,32 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-"""Copies files from CIPD."""
+"""Copies files from CIPD to a specified directory.
+
+By default, Pigweed installs packages from a manifest file to a CIPD
+subdirectory as part of environment setup. This script will copy files from this
+directory into a specified output directory.
+
+Here's an example of how to use this script:
+
+Let's say you have a package with a static library:
+
+CIPD path: `pigweed/third_party/libsomething`
+Files:
+  ./libsomething/include/something.h
+  ./libsomething/libsomething.a
+
+And this package was referenced in my_project_packages.json, which was provided
+as a --cipd-package-file in your bootstrap script.
+
+To copy the static libraryto $PW_PROJECT_ROOT/static_libraries, you'd have an
+invocation something like this:
+
+copy_from_cipd --package-name=pigweed/third_party/libsomething \
+               --mainfest=$PW_PROJECT_ROOT/tools/my_project_packages.json \
+               --file=libsomething/libsomething.a \
+               --out=$PW_PROJECT_ROOT/static_libraries
+"""
 
 import argparse
 import json
@@ -20,34 +45,47 @@ import os
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Copy file from CIPD')
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--verbose',
                         '-v',
                         help='Verbose output',
                         action='store_true')
-    parser.add_argument('--manifest', help='Path to CIPD JSON file')
-    parser.add_argument('--out', help='Output folder', default='.')
+    parser.add_argument('--manifest',
+                        required=True,
+                        type=Path,
+                        help='Path to CIPD JSON manifest file')
+    parser.add_argument('--out-dir',
+                        type=Path,
+                        default='.',
+                        help='Output folder to copy the specified file to')
     parser.add_argument('--package-name',
                         required=True,
-                        help='Basename of CIPD package')
-    # TODO(pwbug/334) Support multiple values for --file-name.
-    parser.add_argument('--file-name',
+                        help='The CIPD package name')
+    # TODO(pwbug/334) Support multiple values for --file.
+    parser.add_argument('--file',
                         required=True,
-                        help='Basename of file in CIPD package')
-    parser.add_argument('--cipd-var',
-                        required=True,
-                        help='Name of CIPD directory environment variable')
+                        type=Path,
+                        help='Path of the file to copy from the CIPD package. '
+                        'This is relative to the CIPD package root of the '
+                        'provided manifest.')
+    parser.add_argument('--cipd-package-root',
+                        type=Path,
+                        help="Path to the root of the package's install "
+                        'directory. This is usually at '
+                        'PW_{manifest name}_CIPD_INSTALL_DIR')
     return parser.parse_args()
 
 
 def check_version(manifest, cipd_path, package_name):
+    base_package_name = os.path.basename(package_name)
     instance_id_path = os.path.join(cipd_path, '.versions',
-                                    f'{package_name}.cipd_version')
+                                    f'{base_package_name}.cipd_version')
     with open(instance_id_path, 'r') as ins:
         instance_id = json.load(ins)['instance_id']
 
@@ -76,18 +114,23 @@ def main():
     if args.verbose:
         logger.setLevel(logging.DEBUG)
 
-    try:
-        cipd_path = os.environ[args.cipd_var]
-    except KeyError:
-        logger.error(
-            "The %s environment variable isn't set. Did you forget to run "
-            '`. ./bootstrap.sh`?', args.cipd_var)
-        sys.exit(1)
+    # Try to infer CIPD install root from the manifest name.
+    if args.cipd_package_root is None:
+        file_base_name = args.manifest.stem
+        args.cipd_var = 'PW_{}_CIPD_INSTALL_DIR'.format(file_base_name.upper())
+        try:
+            args.cipd_package_root = os.environ[args.cipd_var]
+        except KeyError:
+            logger.error(
+                "The %s environment variable isn't set. Did you forget to run "
+                '`. ./bootstrap.sh`? Is the %s manifest installed to a '
+                'different path?', args.cipd_var, file_base_name)
+            sys.exit(1)
 
-    check_version(args.manifest, cipd_path, args.package_name)
+    check_version(args.manifest, args.cipd_package_root, args.package_name)
 
-    shutil.copyfile(os.path.join(cipd_path, args.file_name),
-                    os.path.join(args.out, args.file_name))
+    shutil.copyfile(os.path.join(args.cipd_package_root, args.file),
+                    os.path.join(args.out_dir, args.file))
 
 
 if __name__ == '__main__':
