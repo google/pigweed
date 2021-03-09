0c84ab6698c8b406f8877176623432a5f34c5f92
Wyatt Hepler <hepler@google.com>
2021-03-09 08:37:19 -0800

    pw_build: Update pw_error template
    - Display full target names, including the toolchain, for build errors.
    - Print the dependency path that caused the error using gn path.
    - Add message_lines argument to simplify multi-line messages.
    - Replace null_backend.py with pw_error.
    - Ensure that pw_error works without the Pigweed Python packages
      installed.
    
    Example error if pw_log_BACKEND is unset:
    
    9:28:27 ERR
    9:28:27 ERR Build error for //pw_log:pw_log.NO_BACKEND_SET(//targets/host:host_clang_debug):
    9:28:27 ERR
    9:28:27 ERR   Attempted to build the //pw_log:pw_log facade with no backend.
    9:28:27 ERR
    9:28:27 ERR   If you are using this facade, ensure you have configured a backend
    9:28:27 ERR   properly. The build arg for the facade must be set to a valid
    9:28:27 ERR   backend in the toolchain. For example, you may need to add a line
    9:28:27 ERR   like the following to the toolchain's .gni file:
    9:28:27 ERR
    9:28:27 ERR     pw_log_BACKEND = "//path/to/the:backend"
    9:28:27 ERR
    9:28:27 ERR   If you are NOT using this facade, this error may have been triggered
    9:28:27 ERR   by trying to build all targets.
    9:28:27 ERR
    9:28:28 ERR Dependency path to this target:
    9:28:28 ERR
    9:28:28 ERR   gn path out //:default "//pw_log:pw_log.NO_BACKEND_SET(//targets/host:host_clang_debug)"
    //:default --[private]-->
    //:host --[private]-->
    //:pigweed_default(//targets/host:host_clang_debug) --[private]-->
    //pw_trace:trace_example_basic(//targets/host:host_clang_debug) --[private]-->
    //pw_log:pw_log(//targets/host:host_clang_debug) --[public]-->
    //pw_log:pw_log.NO_BACKEND_SET(//targets/host:host_clang_debug)
    
    Showing one of 75 "interesting" non-data paths. 0 of them are public.
    Use --all to print all paths.
    
    Fixed: 336
    Change-Id: Ida252e1ae9956822ec0efbbfc82bad60f58de5e8
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/36240
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Keir Mierle <keir@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>

--- pw_build/py/pw_build/error.py
+++ pw_build/py/pw_build/error.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The Pigweed Authors
+# Copyright 2021 The Pigweed Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -15,32 +15,70 @@
 
 import argparse
 import logging
+import os
+from pathlib import Path
+import subprocess
 import sys
 
-import pw_cli.log
+try:
+    from pw_cli.log import install as setup_logging
+except ImportError:
+    from logging import basicConfig as setup_logging  # type: ignore
 
 _LOG = logging.getLogger(__name__)
 
 
-def _parse_args():
+def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--message', help='Error message to print')
-    parser.add_argument('--target', help='GN target in which error occurred')
+    parser.add_argument('--message',
+                        required=True,
+                        help='Error message to print')
+    parser.add_argument('--target',
+                        required=True,
+                        help='GN target in which the error occurred')
+    parser.add_argument('--root', required=True, type=Path, help='GN root')
+    parser.add_argument('--out', required=True, type=Path, help='GN out dir')
     return parser.parse_args()
 
 
-def main(message: str, target: str) -> int:
+def main(message: str, target: str, root: Path, out: Path) -> int:
+    """Logs the error message and returns 1."""
+
     _LOG.error('')
-    _LOG.error('Build error:')
+    _LOG.error('Build error for %s:', target)
     _LOG.error('')
+
     for line in message.split('\\n'):
         _LOG.error('  %s', line)
+
     _LOG.error('')
-    _LOG.error('(in %s)', target)
-    _LOG.error('')
+
+    gn_cmd = subprocess.run(
+        ['gn', 'path', f'--root={root}', out, '//:default', target],
+        capture_output=True)
+    path_info = gn_cmd.stdout.decode(errors='replace').rstrip()
+
+    relative_out = os.path.relpath(out, root)
+
+    if gn_cmd.returncode == 0 and 'No non-data paths found' not in path_info:
+        _LOG.error('Dependency path to this target:')
+        _LOG.error('')
+        _LOG.error('  gn path %s //:default "%s"\n%s', relative_out, target,
+                   path_info)
+        _LOG.error('')
+    else:
+        _LOG.error(
+            'Run this command to see the build dependency path to this target:'
+        )
+        _LOG.error('')
+        _LOG.error('  gn path %s <target> "%s"', relative_out, target)
+        _LOG.error('')
+        _LOG.error('where <target> is the GN target you are building.')
+        _LOG.error('')
+
     return 1
 
 
 if __name__ == '__main__':
-    pw_cli.log.install()
+    setup_logging()
     sys.exit(main(**vars(_parse_args())))
