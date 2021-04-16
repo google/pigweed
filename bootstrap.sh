18f1e9b753af8ddf25860b09666d1b5323e9f469
Rob Mohr <mohrr@google.com>
2021-04-16 15:07:04 -0700

    pw_env_setup: Switch from 'which' to 'command -v'
    Change-Id: Ia24247a9a0580b88ccf57f134c8796951972797a
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/41345
    Pigweed-Auto-Submit: Rob Mohr <mohrr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>
    Reviewed-by: Paul Mathieu <paulmathieu@google.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@
 # This script must be tested on bash, zsh, and dash.
 
 _bootstrap_abspath () {
-  $(which python || which python3 || which python2) -c "import os.path; print(os.path.abspath('$@'))"
+  $(command -v python || command -v python3 || command -v python2) -c "import os.path; print(os.path.abspath('$@'))"
 }
 
 # Users are not expected to set PW_CHECKOUT_ROOT, it's only used because it
