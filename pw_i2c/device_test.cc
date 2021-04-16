6e15a13e9e4ecdb24928fd394ea63a6e5360d2a0
Kevin Zeng <zengk@google.com>
2021-04-16 00:34:17 -0700

    pw_i2c: Add Device class
    This object will wrap the initiator along with an address.
    Device is a high level wrapper to be used to stream the reading
    or writing of arbitrary data to the device.
    
    Testing:
    Host test -- OK
    
    Change-Id: I28afd9a956917b28d4db04e8b1be2788981b12c7
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/41163
    Reviewed-by: Ewout van Bekkum <ewout@google.com>
    Pigweed-Auto-Submit: Ewout van Bekkum <ewout@google.com>
    Commit-Queue: Kevin Zeng <zengk@google.com>

---
+++ pw_i2c/device_test.cc
@@ -0,0 +1,52 @@
+// Copyright 2021 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+#include "pw_i2c/device.h"
+
+#include "gtest/gtest.h"
+#include "pw_bytes/byte_builder.h"
+
+namespace pw {
+namespace i2c {
+namespace {
+
+// Dummy test initiator that's used for testing.
+class TestInitiator : public Initiator {
+ public:
+  explicit TestInitiator() {}
+
+ private:
+  Status DoWriteReadFor(Address,
+                        ConstByteSpan,
+                        ByteSpan,
+                        chrono::SystemClock::duration) override {
+    // Empty implementation.
+    return OkStatus();
+  }
+
+  ByteBuffer<10> write_buffer_;
+  ByteBuffer<10> read_buffer_;
+};
+
+// This test just checks to make sure the Device object compiles.
+// TODO(b/185609270): Full test coverage.
+TEST(DeviceCompilationTest, CompileOk) {
+  constexpr Address kDummyDeviceAddress = Address::SevenBit<0x3F>();
+
+  TestInitiator initiator;
+  Device device = Device(initiator, kDummyDeviceAddress);
+}
+
+}  // namespace
+}  // namespace i2c
+}  // namespace pw
--- pw_i2c/docs.rst
+++ pw_i2c/docs.rst
@@ -18,3 +18,10 @@ pw::i2c::Initiator
 The common interface for initiating transactions with devices on an I2C bus.
 Other documentation sources may call this style of interface an I2C "master",
 "central" or "controller".
+
+pw::i2c::Device
+---------------
+The common interface for interfacing with generic I2C devices. This object
+contains ``pw::i2c::Address`` and wraps the ``pw::i2c::Initiator`` API.
+Common use case includes streaming arbitrary data (Read/Write). Only works
+with devices with a single device address.
