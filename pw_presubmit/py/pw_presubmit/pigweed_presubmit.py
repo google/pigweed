3acfd0fae2604b10530e8cf56c478424b5ed7863
Ali Zhang <alizhang@google.com>
2021-07-20 16:22:35 -0700

    pw_presubmit: Add gn_crypto_boringssl_build
    Test: `pw presubmit --step gn_crypto_boringssl_build`
    
    No-Docs-Update-Reason: N/A
    
    Change-Id: I32133ecee66f2b4f8928d11c7d32440bf29c55e4
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/53862
    Pigweed-Auto-Submit: Ali Zhang <alizhang@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>
    Reviewed-by: Rob Mohr <mohrr@google.com>

--- pw_presubmit/py/pw_presubmit/pigweed_presubmit.py
+++ pw_presubmit/py/pw_presubmit/pigweed_presubmit.py
@@ -166,6 +166,18 @@ def gn_crypto_mbedtls_build(ctx: PresubmitContext):
 
 
 @filter_paths(endswith=_BUILD_EXTENSIONS)
+def gn_crypto_boringssl_build(ctx: PresubmitContext):
+    build.install_package(ctx.package_root, 'boringssl')
+    build.gn_gen(ctx.root,
+                 ctx.output_dir,
+                 dir_pw_third_party_boringssl='"{}"'.format(ctx.package_root /
+                                                            'boringssl'),
+                 pw_crypto_SHA256_BACKEND='"{}"'.format(
+                     ctx.root / 'pw_crypto:sha256_boringssl'))
+    build.ninja(ctx.output_dir)
+
+
+@filter_paths(endswith=_BUILD_EXTENSIONS)
 def gn_teensy_build(ctx: PresubmitContext):
     build.install_package(ctx.package_root, 'teensy')
     build.gn_gen(ctx.root,
@@ -724,6 +736,7 @@ OTHER_CHECKS = (
     gn_boringssl_build,
     gn_nanopb_build,
     gn_crypto_mbedtls_build,
+    gn_crypto_boringssl_build,
     gn_full_build_check,
     gn_full_qemu_check,
     gn_clang_build,
