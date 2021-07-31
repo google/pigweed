2253040d12b3133e374d1814673f82d6f09f7565
Wyatt Hepler <hepler@google.com>
2021-07-30 23:40:40 -0700

    pw_transfer: Python & C++ integration test
    Basic tests for transmitting data between a Python client and a C++
    server using pw_transfer.
    
    Change-Id: I75c361ec100b055e0d6afb0091e64cc9d773c568
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/54707
    Commit-Queue: Wyatt Hepler <hepler@google.com>
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Alexei Frolov <frolv@google.com>

---
+++ pw_transfer/py/tests/python_cpp_transfer_test.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Pigweed Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Tests transfers between the Python client and C++ service."""
+
+from typing import List, Tuple
+import unittest
+
+from pw_hdlc import rpc
+from pw_rpc import testing
+from pw_transfer import transfer, transfer_pb2
+
+PORT = 33001
+TRANSFER_ID = 99
+ITERATIONS = 5
+
+
+class TransferServiceIntegrationTest(unittest.TestCase):
+    """Tests transfers between the Python client and C++ service."""
+    test_server_command: Tuple[str, ...] = ()
+
+    def setUp(self) -> None:
+        context = rpc.HdlcRpcLocalServerAndClient(self.test_server_command,
+                                                  PORT, [transfer_pb2])
+        self._server = context.server
+        service = context.client.channel(1).rpcs.pw.transfer.Transfer
+        self.manager = transfer.Manager(service)
+
+    def set_contents(self, data: str) -> None:
+        assert self._server.stdin is not None
+        self._server.stdin.write(data.encode() + b'\0')
+        self._server.stdin.flush()
+
+    def tearDown(self) -> None:
+        if hasattr(self, '_server'):
+            self._server.close()
+
+    def test_read_empty(self) -> None:
+        for _ in range(ITERATIONS):
+            self.assertEqual(self.manager.read(TRANSFER_ID), b'')
+
+    def test_read_single_byte(self) -> None:
+        self.set_contents('\1')
+
+        for _ in range(ITERATIONS):
+            self.assertEqual(self.manager.read(TRANSFER_ID), b'\1')
+
+    def test_read_small_amount_of_data(self) -> None:
+        self.set_contents('hunter2')
+
+        for _ in range(ITERATIONS):
+            self.assertEqual(self.manager.read(TRANSFER_ID), b'hunter2')
+
+    def test_read_large_amount_of_data(self) -> None:
+        self.set_contents('~' * 512)
+
+        for _ in range(ITERATIONS):
+            self.assertEqual(self.manager.read(TRANSFER_ID), b'~' * 512)
+
+
+def _main(test_server_command: List[str], unittest_args: List[str]) -> None:
+    TransferServiceIntegrationTest.test_server_command = tuple(
+        test_server_command)
+    unittest.main(argv=unittest_args)
+
+
+if __name__ == '__main__':
+    _main(**vars(testing.parse_test_server_args()))
---
+++ pw_transfer/test_rpc_server.cc
@@ -0,0 +1,106 @@
+// Copyright 2021 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// Simple RPC server with the transfer service registered. Reads HDLC frames
+// with RPC packets through a socket. The transfer service reads and writes to a
+// fixed-size buffer. The buffer's contents can be set by writing
+// null-terminated strings through stdin.
+
+#include <cstddef>
+#include <thread>
+#include <variant>
+
+#include "pw_assert/check.h"
+#include "pw_log/log.h"
+#include "pw_rpc_system_server/rpc_server.h"
+#include "pw_rpc_system_server/socket.h"
+#include "pw_stream/memory_stream.h"
+#include "pw_transfer/transfer.h"
+
+namespace pw::transfer {
+namespace {
+
+class BufferReaderWriter final : public ReadWriteHandler {
+ public:
+  BufferReaderWriter(uint32_t transfer_id, ByteSpan buffer)
+      : ReadWriteHandler(transfer_id), buffer_(buffer), size_bytes_(0) {}
+
+  Status PrepareRead() override {
+    set_reader(
+        stream_.emplace<stream::MemoryReader>(buffer_.first(size_bytes_)));
+    return OkStatus();
+  }
+
+  Status PrepareWrite() override {
+    set_writer(stream_.emplace<stream::MemoryWriter>(buffer_));
+    return OkStatus();
+  }
+
+  Status FinalizeWrite(Status) override {
+    size_bytes_ = std::get<stream::MemoryWriter>(stream_).bytes_written();
+    return OkStatus();
+  }
+
+  void set_size(size_t new_size) { size_bytes_ = new_size; }
+
+ private:
+  // TODO(hepler): Use a seekable MemoryReaderWriter when available.
+  std::variant<std::monostate, stream::MemoryReader, stream::MemoryWriter>
+      stream_;
+  ByteSpan buffer_;
+  size_t size_bytes_;
+};
+
+TransferService transfer_service;
+
+std::byte buffer[512];
+
+void RunServer() {
+  rpc::system_server::set_socket_port(33001);
+
+  BufferReaderWriter transfer(99, buffer);
+
+  // Read characters from stdin directly into the buffer. Stop at 0 or overflow.
+  std::thread([&transfer] {
+    int next_char;
+    size_t index = 0;
+
+    while ((next_char = std::getchar()) != EOF) {
+      if (next_char == '\0') {
+        transfer.set_size(index);
+        index = 0;
+        continue;
+      }
+
+      PW_CHECK_UINT_LT(index, sizeof(buffer), "Sent too many characters!");
+      buffer[index++] = static_cast<std::byte>(next_char);
+    }
+  }).detach();
+
+  transfer_service.RegisterHandler(transfer);
+
+  rpc::system_server::Init();
+  rpc::system_server::Server().RegisterService(transfer_service);
+
+  PW_LOG_INFO("Starting pw_rpc server");
+  rpc::system_server::Start();
+}
+
+}  // namespace
+}  // namespace pw::transfer
+
+int main() {
+  pw::transfer::RunServer();
+  return 0;
+}
