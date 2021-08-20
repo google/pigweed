9e8ca38f729ad9507290ce0ef91ca040ce7aea60
Wyatt Hepler <hepler@google.com>
2021-08-20 08:29:57 -0700

    pw_rpc: Log Nanopb errors when encode/decode fails
    When enabled, Nanopb provides helpful error strings when encoding or
    decoding fails. Log the error strings if they're available.
    
    Change-Id: Ibc1ec5cd088b76e0351fdbdb732b2363003d578a
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/57780
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Alexei Frolov <frolv@google.com>
    Reviewed-by: Scott James Remnant <keybuk@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>

--- pw_rpc/nanopb/common.cc
+++ pw_rpc/nanopb/common.cc
@@ -16,8 +16,10 @@
 
 #include "pb_decode.h"
 #include "pb_encode.h"
+#include "pw_log/log.h"
 
 namespace pw::rpc::internal {
+namespace {
 
 // Nanopb 3 uses pb_field_s and Nanopb 4 uses pb_msgdesc_s for fields. The
 // Nanopb version macro is difficult to use, so deduce the correct type from the
@@ -32,12 +34,29 @@ struct NanopbTraits<bool(pb_istream_t*, FieldsType, void*)> {
 
 using Fields = typename NanopbTraits<decltype(pb_decode)>::Fields;
 
+}  // namespace
+
+// PB_NO_ERRMSG is used in pb_decode.h and pb_encode.h to enable or disable the
+// errmsg member of the istream and ostream structs. If errmsg is available, use
+// it to give more detailed log messages.
+#ifdef PB_NO_ERRMSG
+
+#define PW_RPC_LOG_NANOPB_FAILURE(msg, stream) PW_LOG_ERROR(msg)
+
+#else
+
+#define PW_RPC_LOG_NANOPB_FAILURE(msg, stream) \
+  PW_LOG_ERROR(msg ": %s", stream.errmsg)
+
+#endif  // PB_NO_ERRMSG
+
 StatusWithSize NanopbMethodSerde::Encode(NanopbMessageDescriptor fields,
                                          const void* proto_struct,
                                          ByteSpan buffer) const {
   auto output = pb_ostream_from_buffer(
       reinterpret_cast<pb_byte_t*>(buffer.data()), buffer.size());
   if (!pb_encode(&output, static_cast<Fields>(fields), proto_struct)) {
+    PW_RPC_LOG_NANOPB_FAILURE("Nanopb protobuf encode failed", output);
     return StatusWithSize::Internal();
   }
 
@@ -49,7 +68,13 @@ bool NanopbMethodSerde::Decode(NanopbMessageDescriptor fields,
                                void* proto_struct) const {
   auto input = pb_istream_from_buffer(
       reinterpret_cast<const pb_byte_t*>(buffer.data()), buffer.size());
-  return pb_decode(&input, static_cast<Fields>(fields), proto_struct);
+  bool result = pb_decode(&input, static_cast<Fields>(fields), proto_struct);
+  if (!result) {
+    PW_RPC_LOG_NANOPB_FAILURE("Nanopb protobuf decode failed", input);
+  }
+  return result;
 }
+
+#undef PW_RPC_LOG_NANOPB_FAILURE
 
 }  // namespace pw::rpc::internal
