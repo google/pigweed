4eb017ff950da6362fa0e481a340298ae21e2402
David Rogers <davidrogers@google.com>
2021-10-06 14:42:25 -0700

    pw_blob_store: Add testing of deferred writes to unerased flash
    Use a combination of erased and random fille flash partitions for
    deferred write tests. Add several additional tests for different
    discard and erase situations.
    
    Test: verified that test fails before pwrev/64085 and passes after.
    
    Change-Id: Ie37480388733feee272728e6c60922e637fcefa8
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/64300
    Reviewed-by: Ewout van Bekkum <ewout@google.com>
    Commit-Queue: David Rogers <davidrogers@google.com>

--- pw_blob_store/blob_store_deferred_write_test.cc
+++ pw_blob_store/blob_store_deferred_write_test.cc
@@ -33,28 +33,33 @@ class DeferredWriteTest : public ::testing::Test {
  protected:
   DeferredWriteTest() : flash_(kFlashAlignment), partition_(&flash_) {}
 
-  void InitFlashTo(std::span<const std::byte> contents) {
-    partition_.Erase()
-        .IgnoreError();  // TODO(pwbug/387): Handle Status properly
-    std::memcpy(flash_.buffer().data(), contents.data(), contents.size());
+  void InitFlashToErased() { ASSERT_EQ(OkStatus(), partition_.Erase()); }
+
+  void InitFlashToRandom(uint64_t seed) {
+    random::XorShiftStarRng64 rng(seed);
+    StatusWithSize sws = rng.Get(flash_.buffer());
+    ASSERT_EQ(OkStatus(), sws.status());
+    ASSERT_EQ(sws.size(), flash_.buffer().size());
   }
 
   void InitBufferToRandom(uint64_t seed) {
-    partition_.Erase()
-        .IgnoreError();  // TODO(pwbug/387): Handle Status properly
     random::XorShiftStarRng64 rng(seed);
-    rng.Get(buffer_).IgnoreError();  // TODO(pwbug/387): Handle Status properly
+    StatusWithSize sws = rng.Get(buffer_);
+    ASSERT_EQ(OkStatus(), sws.status());
+    ASSERT_EQ(sws.size(), buffer_.size());
   }
 
   void InitBufferToFill(char fill) {
-    partition_.Erase()
-        .IgnoreError();  // TODO(pwbug/387): Handle Status properly
+    ASSERT_EQ(OkStatus(), partition_.Erase());
     std::memset(buffer_.data(), fill, buffer_.size());
   }
 
   // Fill the source buffer with random pattern based on given seed, written to
   // BlobStore in specified chunk size.
-  void ChunkWriteTest(size_t chunk_size, size_t flush_interval) {
+  void ChunkWriteTest(size_t chunk_size,
+                      size_t flush_interval,
+                      bool explicit_discard = false,
+                      bool explicit_erase = false) {
     constexpr size_t kWriteSize = 64;
     kvs::ChecksumCrc16 checksum;
 
@@ -69,6 +74,14 @@ class DeferredWriteTest : public ::testing::Test {
 
     BlobStore::DeferredWriterWithBuffer writer(blob);
     EXPECT_EQ(OkStatus(), writer.Open());
+
+    if (explicit_discard) {
+      EXPECT_EQ(OkStatus(), writer.Discard());
+    }
+
+    if (explicit_erase) {
+      EXPECT_EQ(OkStatus(), writer.Erase());
+    }
 
     ByteSpan source = buffer_;
     while (source.size_bytes() > 0) {
@@ -124,58 +137,88 @@ class DeferredWriteTest : public ::testing::Test {
 };
 
 TEST_F(DeferredWriteTest, ChunkWrite1) {
+  InitFlashToErased();
   InitBufferToRandom(0x8675309);
   ChunkWriteTest(1, 16);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite2) {
+  InitFlashToRandom(0x2283);
   InitBufferToRandom(0x8675);
   ChunkWriteTest(2, 16);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite3) {
+  InitFlashToErased();
   InitBufferToFill(0);
   ChunkWriteTest(3, 16);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite4) {
+  InitFlashToErased();
   InitBufferToFill(1);
   ChunkWriteTest(4, 64);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite5) {
+  InitFlashToErased();
   InitBufferToFill(0xff);
   ChunkWriteTest(5, 64);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite16) {
+  InitFlashToErased();
   InitBufferToRandom(0x86);
   ChunkWriteTest(16, 128);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite64) {
+  InitFlashToRandom(0x9223);
   InitBufferToRandom(0x9);
   ChunkWriteTest(64, 128);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite64FullBufferFill) {
+  InitFlashToErased();
   InitBufferToRandom(0x9);
   ChunkWriteTest(64, kBufferSize);
 }
 
 TEST_F(DeferredWriteTest, ChunkWrite256) {
+  InitFlashToErased();
   InitBufferToRandom(0x12345678);
   ChunkWriteTest(256, 256);
 }
 
-// TODO: test that has dirty flash, invalidated blob, open writer, invalidate
-// (not erase) and start writing (does the auto/implicit erase).
+TEST_F(DeferredWriteTest, ChunkWriteDiscard16) {
+  InitFlashToErased();
+  InitBufferToRandom(0x86);
 
-// TODO: test that has dirty flash, invalidated blob, open writer, explicit
-// erase and start writing.
+  // Test with a discard of an invalid blob and erased flash.
+  ChunkWriteTest(16, 128, true);
 
-// TODO: test start with dirty flash/invalid blob, open writer, write, close.
-// Verifies erase logic when write buffer has contents.
+  // Test with a discard of a valid blob.
+  ChunkWriteTest(16, 128, true);
+
+  // Test with a discard of an current blob with corrupted date.
+  InitFlashToRandom(0x9223);
+  ChunkWriteTest(16, 128, true);
+}
+
+TEST_F(DeferredWriteTest, ChunkWriteErase16) {
+  InitFlashToErased();
+  InitBufferToRandom(0x1286);
+
+  // Test with an erase of an invalid blob and erased flash.
+  ChunkWriteTest(16, 128, false, true);
+
+  // Test with an erase of a valid blob.
+  ChunkWriteTest(16, 128, false, true);
+
+  // Test with an erase of an current blob with corrupted date.
+  InitFlashToRandom(0x9223);
+  ChunkWriteTest(16, 128, false, true);
+}
 
 }  // namespace
 }  // namespace pw::blob_store
