28e1162b719beef6930fa93f08ae17b64b5764f1
Anthony DiGirolamo <tonymd@google.com>
2021-10-28 13:42:41 -0700

    pw_package: Add FreeRTOS
    Requires: pigweed-internal:18940
    Change-Id: Ib65d262e5f4dbb2b68f92a33d4ae396896961f6d
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/66748
    Reviewed-by: Armando Montanez <amontanez@google.com>
    Commit-Queue: Anthony DiGirolamo <tonymd@google.com>

--- pw_package/py/pw_package/pigweed_packages.py
+++ pw_package/py/pw_package/pigweed_packages.py
@@ -16,15 +16,16 @@
 import sys
 
 from pw_package import package_manager
-from pw_package.packages import nanopb
 from pw_package.packages import arduino_core  # pylint: disable=unused-import
-from pw_package.packages import stm32cube  # pylint: disable=unused-import
 from pw_package.packages import boringssl  # pylint: disable=unused-import
 from pw_package.packages import chromium_verifier  # pylint: disable=unused-import
+from pw_package.packages import crlset  # pylint: disable=unused-import
+from pw_package.packages import freertos  # pylint: disable=unused-import
 from pw_package.packages import mbedtls  # pylint: disable=unused-import
 from pw_package.packages import micro_ecc  # pylint: disable=unused-import
-from pw_package.packages import crlset  # pylint: disable=unused-import
+from pw_package.packages import nanopb
 from pw_package.packages import protobuf  # pylint: disable=unused-import
+from pw_package.packages import stm32cube  # pylint: disable=unused-import
 
 
 def initialize():
