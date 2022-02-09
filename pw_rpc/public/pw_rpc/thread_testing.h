5715ba8c76bdd608c723d98fc9c2d2193908f780
Wyatt Hepler <hepler@google.com>
2022-02-08 19:09:05 -0800

    pw_rpc: Fix WaitForPackets()
    - Avoid races in WaitForPackets() by supporting running code after
      setting up the FakeChannelOutput Send() hook.
    - Avoid blocking indefinitely. PW_ASSERT after a timeout instead.
    
    Change-Id: Ia4fdee88d61cf446aa7db41110f3332aaff7a9d8
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/83640
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Keir Mierle <keir@google.com>
    Commit-Queue: Wyatt Hepler <hepler@google.com>

--- pw_rpc/public/pw_rpc/thread_testing.h
+++ pw_rpc/public/pw_rpc/thread_testing.h
@@ -13,12 +13,30 @@
 // the License.
 #pragma once
 
+#include <chrono>
+
+#include "pw_assert/assert.h"
 #include "pw_rpc/internal/fake_channel_output.h"
+#include "pw_sync/counting_semaphore.h"
 
 namespace pw::rpc::test {
 
 // Wait until the provided RawFakeChannelOutput or NanopbFakeChannelOutput
 // receives the specified number of packets.
-void WaitForPackets(internal::test::FakeChannelOutput& output, int count);
+template <unsigned kTimeoutSeconds = 10, typename Function>
+void WaitForPackets(internal::test::FakeChannelOutput& output,
+                    int count,
+                    Function&& run_before) {
+  sync::CountingSemaphore sem;
+  output.set_on_send([&sem](ConstByteSpan, Status) { sem.release(); });
+
+  run_before();
+
+  for (int i = 0; i < count; ++i) {
+    PW_ASSERT(sem.try_acquire_for(std::chrono::seconds(kTimeoutSeconds)));
+  }
+
+  output.set_on_send(nullptr);
+}
 
 }  // namespace pw::rpc::test
