200aeb31d64d88ebc0219e028fbabd6d1d279035
RJ Ascani <rjascani@google.com>
2022-03-25 16:42:59 -0700

    pw_rpc: Add SynchronousCall wrappers for nanopb
    This CL adds SynchronousCall wrappers for making client RPC calls using
    nanopb. It includes variations for indefinite waiting, deadline waiting
    and timeout waiting. These calls return a SynchronousCallResult, which
    holds the call status and, if the server responded, the response. If the
    call terminated prior to a server response, the result object can be
    queried to determine what type of error occurred.
    
    Change-Id: I6947e703c3a5891a00c0f368e59992ed3ab49fc9
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/89120
    Commit-Queue: RJ Ascani <rjascani@google.com>
    Reviewed-by: Wyatt Hepler <hepler@google.com>

--- pw_rpc/py/pw_rpc/codegen.py
+++ pw_rpc/py/pw_rpc/codegen.py
@@ -273,6 +273,9 @@ def _generate_info(gen: CodeGenerator, namespace: str,
 
             gen.line('}')
 
+            gen.line(f'using GeneratedClient = ::{namespace}'
+                     f'::pw_rpc::{gen.name()}::{service.name()}::Client;')
+
             gen.method_info_specialization(method)
 
         gen.line('};')
