cc281233334ca4c0a06e4d419786a721e41e1c9b
Trinity Lundgren <trinit@google.com>
2022-04-13 00:44:56 +0000

    pw_i2c: Add device test suite
    - Use mock initiator instead of mock class inside device_test
    
    Fixed: b/185609270
    Change-Id: I6fbeee17fca989bb509db36732d5494386b3e150
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/90980
    Reviewed-by: Kevin Zeng <zengk@google.com>
    Reviewed-by: Paul Mathieu <paulmathieu@google.com>
    Commit-Queue: Trinity Lundgren <trinit@google.com>

--- pw_i2c/device_test.cc
+++ pw_i2c/device_test.cc
@@ -11,42 +11,83 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
+
 #include "pw_i2c/device.h"
+
+#include <chrono>
 
 #include "gtest/gtest.h"
 #include "pw_bytes/byte_builder.h"
+#include "pw_i2c/initiator_mock.h"
 
-namespace pw {
-namespace i2c {
+using namespace std::literals::chrono_literals;
+
+namespace pw::i2c {
 namespace {
 
-// Fake test initiator that's used for testing.
-class TestInitiator : public Initiator {
- public:
-  explicit TestInitiator() {}
-
- private:
-  Status DoWriteReadFor(Address,
-                        ConstByteSpan,
-                        ByteSpan,
-                        chrono::SystemClock::duration) override {
-    // Empty implementation.
-    return OkStatus();
-  }
-
-  ByteBuffer<10> write_buffer_;
-  ByteBuffer<10> read_buffer_;
-};
-
-// This test just checks to make sure the Device object compiles.
-// TODO(b/185609270): Full test coverage.
-TEST(DeviceCompilationTest, CompileOk) {
+TEST(Device, WriteReadForOk) {
   constexpr Address kTestDeviceAddress = Address::SevenBit<0x3F>();
 
-  TestInitiator initiator;
+  constexpr auto kExpectWrite1 = bytes::Array<1, 2, 3>();
+  constexpr auto kExpectRead1 = bytes::Array<1, 2>();
+
+  auto expected_transactions = MakeExpectedTransactionArray({Transaction(
+      OkStatus(), kTestDeviceAddress, kExpectWrite1, kExpectRead1, 2ms)});
+
+  MockInitiator initiator(expected_transactions);
+
   Device device = Device(initiator, kTestDeviceAddress);
+
+  std::array<std::byte, kExpectRead1.size()> read1;
+  EXPECT_EQ(device.WriteReadFor(kExpectWrite1, read1, 2ms), OkStatus());
+  EXPECT_TRUE(std::equal(
+      read1.begin(), read1.end(), kExpectRead1.begin(), kExpectRead1.end()));
+  EXPECT_EQ(initiator.Finalize(), OkStatus());
+}
+
+TEST(Device, WriteForOk) {
+  constexpr Address kTestDeviceAddress = Address::SevenBit<0x3F>();
+
+  constexpr auto kExpectWrite1 = bytes::Array<1, 2, 3>();
+
+  auto expected_transactions = MakeExpectedTransactionArray(
+      {WriteTransaction(OkStatus(), kTestDeviceAddress, kExpectWrite1, 2ms)});
+
+  MockInitiator initiator(expected_transactions);
+  Device device = Device(initiator, kTestDeviceAddress);
+
+  EXPECT_EQ(device.WriteFor(kExpectWrite1, 2ms), OkStatus());
+  EXPECT_EQ(initiator.Finalize(), OkStatus());
+}
+
+TEST(Device, ReadForOk) {
+  constexpr Address kTestDeviceAddress = Address::SevenBit<0x3F>();
+
+  constexpr auto kExpectRead1 = bytes::Array<1, 2, 3>();
+
+  auto expected_transactions = MakeExpectedTransactionArray(
+      {ReadTransaction(OkStatus(), kTestDeviceAddress, kExpectRead1, 2ms)});
+
+  MockInitiator initiator(expected_transactions);
+  Device device = Device(initiator, kTestDeviceAddress);
+
+  std::array<std::byte, kExpectRead1.size()> read1;
+  EXPECT_EQ(device.ReadFor(read1, 2ms), OkStatus());
+  EXPECT_EQ(initiator.Finalize(), OkStatus());
+}
+
+TEST(Device, ProbeDeviceForOk) {
+  constexpr Address kTestDeviceAddress = Address::SevenBit<0x3F>();
+
+  auto expected_transactions = MakeExpectedTransactionArray(
+      {ProbeTransaction(OkStatus(), kTestDeviceAddress, 2ms)});
+
+  MockInitiator initiator(expected_transactions);
+  Device device = Device(initiator, kTestDeviceAddress);
+
+  EXPECT_EQ(initiator.ProbeDeviceFor(kTestDeviceAddress, 2ms), OkStatus());
+  EXPECT_EQ(initiator.Finalize(), OkStatus());
 }
 
 }  // namespace
-}  // namespace i2c
-}  // namespace pw
+}  // namespace pw::i2c
