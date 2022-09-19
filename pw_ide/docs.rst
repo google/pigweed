bcd53dbe356f9b85e05a753d37f5bafc44fdff3a
Chad Norvell <chadnorvell@google.com>
2022-09-19 18:53:13 +0000

    pw_ide: CLI tools for C++ comp. DBs
    This surfaces a set of CLI commands for accessing the C++/clangd
    compilation database management features.
    
    This also scaffolds the git-subcommand-like structure of the `pw ide`
    CLI.
    
    Change-Id: Ia6cf86aa91ff51d10f5fc1cfaef742ce46eebdcf
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/110255
    Commit-Queue: Chad Norvell <chadnorvell@google.com>
    Reviewed-by: Anthony DiGirolamo <tonymd@google.com>

--- pw_ide/docs.rst
+++ pw_ide/docs.rst
@@ -22,3 +22,49 @@ these options can be configured:
   target. For example, GN outputs build artifacts for the
   ``pw_strict_host_clang_debug`` target in a directory with that name in the
   ``out`` directory. So that becomes the canonical name for that target.
+
+C++ Code Intelligence via ``clangd``
+====================================
+`clangd <https://clangd.llvm.org/>`_ is a language server that provides C/C++
+code intelligence features to any editor that supports the language server
+protocol (LSP). It uses a
+`compilation database <https://clang.llvm.org/docs/JSONCompilationDatabase.html>`_,
+a JSON file containing the compile commands for the project. Projects that have
+multiple targets and/or use multiple toolchains need separate compilation
+databases for each target/toolchain. ``pw_ide`` provides tools for managing
+those databases.
+
+Assuming you have a compilation database output from a build system, start with:
+
+.. code-block:: bash
+
+  pw ide cpp --process <path to your compile_commands.json>
+
+The ``pw_ide`` working directory will now contain one or more compilation
+database files, each for a separate target among the targets defined in
+``.pw_ide.yaml``. List the available targets with:
+
+.. code-block:: bash
+
+  pw ide cpp --list
+
+Then set the target that ``clangd`` should use with:
+
+.. code-block:: bash
+
+  pw ide cpp --set <selected target name>
+
+``clangd`` can now be configured to point to the ``compile_commands.json`` file
+in the ``pw_ide`` working directory and provide code intelligence for the
+selected target. If you select a new target, ``clangd`` *does not* need to be
+reconfigured to look at a new file (in other words, ``clangd`` can always be
+pointed at the same, stable ``compile_commands.json`` file). However,
+``clangd`` may need to be restarted when the target changes.
+
+``clangd`` must be run within the activated Pigweed environment in order for
+correct toolchain paths and sysroots to be detected. If you launch your editor
+from the terminal in an activated environment, then nothing special needs to be
+done (e.g. running ``vim`` from the terminal). But if you launch your editor
+outside of the activated environment (e.g. launching Visual Studio Code from
+your GUI shell's launcher), you will need to use the included wrapper
+``clangd.sh`` instead of directly using the ``clangd`` binary.
---
+++ pw_ide/py/pw_ide/commands.py
@@ -0,0 +1,112 @@
+# Copyright 2022 The Pigweed Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""pw_ide CLI command implementations."""
+
+from pathlib import Path
+import sys
+from typing import Optional
+
+from pw_ide.cpp import (get_defined_available_targets, get_target,
+                        process_compilation_database, set_target,
+                        write_compilation_databases)
+
+from pw_ide.exceptions import (BadCompDbException, InvalidTargetException,
+                               MissingCompDbException)
+
+from pw_ide.settings import IdeSettings
+
+
+def _print_current_target(settings: IdeSettings) -> None:
+    print('Current C/C++ language server analysis target: '
+          f'{get_target(settings)}\n')
+
+
+def _print_defined_available_targets(settings: IdeSettings) -> None:
+    print('C/C++ targets available for language server analysis:')
+
+    for toolchain in sorted(get_defined_available_targets(settings)):
+        print(f'\t{toolchain}')
+
+    print('')
+
+
+def cmd_cpp(
+    should_list_targets: bool,
+    target_to_set: Optional[str],
+    compdb_file_path: Optional[Path],
+    override_current_target: bool = True,
+    settings: IdeSettings = IdeSettings()
+) -> None:
+    """Configure C/C++ IDE support for Pigweed projects.
+
+    Provides tools for processing C/C++ compilation databases and setting the
+    particular target/toochain to use for code analysis."""
+    default = True
+
+    if should_list_targets:
+        default = False
+        _print_defined_available_targets(settings)
+
+    # Order of operations matters here. It should be possible to process a
+    # compilation database then set successfully set the target in a single
+    # command.
+    if compdb_file_path is not None:
+        default = False
+        try:
+            write_compilation_databases(
+                process_compilation_database(
+                    compdb_file_path,
+                    settings,
+                ),
+                settings,
+            )
+
+            print(
+                f'Processed {str(compdb_file_path)} to {settings.working_dir}')
+        except MissingCompDbException:
+            print(f'File not found: {str(compdb_file_path)}')
+            sys.exit(1)
+        except BadCompDbException:
+            print('File does not match compilation database format: '
+                  f'{str(compdb_file_path)}')
+            sys.exit(1)
+
+    if target_to_set is not None:
+        default = False
+        should_set_target = get_target(settings) is None \
+            or override_current_target
+
+        if should_set_target:
+            try:
+                set_target(target_to_set, settings)
+            except InvalidTargetException:
+                print(f'Invalid target! {target_to_set} not among the '
+                      'available defined targets.\n\n'
+                      'Check .pw_ide.yaml or .pw_ide.user.yaml for defined '
+                      'targets.')
+                sys.exit(1)
+            except MissingCompDbException:
+                print(f'File not found for target! {target_to_set}\n'
+                      'Did you run pw ide cpp --process '
+                      '{path to compile_commands.json}?')
+                sys.exit(1)
+
+            print('Set C/C++ language server analysis target to: '
+                  f'{target_to_set}')
+        else:
+            print('Target already is set and will not be overridden.')
+            _print_current_target(settings)
+
+    if default:
+        _print_current_target(settings)
