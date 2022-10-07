d279c5ee1350ef667432a4bd27c7efbef79a6f92
Fuchsia Authors <noreply@google.com>
2022-10-07 15:33:52 +0000

    third_party/fuchsia: Copybara import of the fit library
      - ecb8d1df3d42d4ca53fe550159aef0fe5849d633 [stdcompat] Fix unused parameter error
      - b367a04b68e7e146fda2b4f671ee23322d13a662 [fit] Rename _internal.h headers to use internal/ directory
    
    GitOrigin-RevId: ecb8d1df3d42d4ca53fe550159aef0fe5849d633
    Change-Id: I639aeb433656196dfa1f48c645c85e54e6ca2926
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/113317
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>
    Reviewed-by: Rob Mohr <mohrr@google.com>

--- third_party/fuchsia/repo/sdk/lib/fit/include/lib/fit/internal/function.h
+++ third_party/fuchsia/repo/sdk/lib/fit/include/lib/fit/internal/function.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_FIT_INCLUDE_LIB_FIT_FUNCTION_INTERNAL_H_
-#define LIB_FIT_INCLUDE_LIB_FIT_FUNCTION_INTERNAL_H_
+#ifndef LIB_FIT_INCLUDE_LIB_FIT_INTERNAL_FUNCTION_H_
+#define LIB_FIT_INCLUDE_LIB_FIT_INTERNAL_FUNCTION_H_
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -15,7 +15,7 @@
 #include <type_traits>
 #include <utility>
 
-#include "nullable.h"
+#include "../nullable.h"
 #include "pw_assert/assert.h"
 #include "pw_preprocessor/compiler.h"
 
@@ -549,4 +549,4 @@ class function_base<inline_target_size, require_inline, Result(Args...)>
 }  // namespace internal
 }  // namespace fit
 
-#endif  // LIB_FIT_INCLUDE_LIB_FIT_FUNCTION_INTERNAL_H_
+#endif  // LIB_FIT_INCLUDE_LIB_FIT_INTERNAL_FUNCTION_H_
