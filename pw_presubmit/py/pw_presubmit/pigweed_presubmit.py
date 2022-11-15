0bfac57edb6cb229e4b28cfe9fdb0c6b1ae6ed4b
Ted Pudlik <tpudlik@google.com>
2022-11-15 17:20:04 +0000

    pw_log: Get bazel wildcard build to work
    Bug: 244743459
    Change-Id: Ifbf4d379b406a681f0fbf4f36e1353adc6c2b5df
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/119393
    Reviewed-by: Anthony DiGirolamo <tonymd@google.com>
    Commit-Queue: Ted Pudlik <tpudlik@google.com>

--- pw_presubmit/py/pw_presubmit/pigweed_presubmit.py
+++ pw_presubmit/py/pw_presubmit/pigweed_presubmit.py
@@ -445,8 +445,6 @@ _TARGETS_THAT_DO_NOT_BUILD_WITH_BAZEL = (
     '-//pw_cpu_exception_cortex_m/...:all',
     '-//pw_crypto/...:all',  # TODO(b/236321905) Remove when passing.
     '-//pw_i2c_mcuxpresso/...:all',
-    '-//pw_log:log_proto_py_pb2',  # TODO(b/241456982) Remove when passing.
-    '-//pw_log:log_proto_py_pb2_genproto',
     '-//pw_system/...:all',
     '-//pw_thread/py/...:all',
     '-//pw_thread:thread_proto_py_pb2',
