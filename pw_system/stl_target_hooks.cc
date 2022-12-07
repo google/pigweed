4f3608a2d723d4de6dc46b2a52690da7d5c465d3
Tom Craig <tommycraig@gmail.com>
2022-12-07 19:35:47 +0000

    pw_system: Move STL main() to toolchain
    pw_system stashed a main() implementation in stl_target_hooks.cc,
    which was a bit of a kludge and caused some downstream challenges.
    
    This commit moves that main() implementation to the
    host_device_simulator toolchain, which also improves symmetry between
    targets.
    
    Longer term, that main() implementation may want to move to a
    newly-created pw_boot_stl module to further improve symmetry.
    
    Change-Id: I6bfb02c76c78249842d76071889f8fb77888c0e3
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/122830
    Reviewed-by: Armando Montanez <amontanez@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>
    Pigweed-Auto-Submit: Armando Montanez <amontanez@google.com>

--- pw_system/stl_target_hooks.cc
+++ pw_system/stl_target_hooks.cc
@@ -12,11 +12,6 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-#define PW_LOG_MODULE_NAME "SYS"
-
-#include "pw_log/log.h"
-#include "pw_system/init.h"
-#include "pw_thread/sleep.h"
 #include "pw_thread/thread.h"
 #include "pw_thread_stl/options.h"
 
@@ -38,16 +33,3 @@ const thread::Options& WorkQueueThreadOptions() {
 }
 
 }  // namespace pw::system
-
-extern "C" int main() {
-  pw::system::Init();
-  // Sleep loop rather than return on this thread so the process isn't closed.
-  while (true) {
-    pw::this_thread::sleep_for(std::chrono::seconds(10));
-    // It's hard to tell that simulator is alive and working since nothing is
-    // logging after initial "boot," so for now log a line occasionally so
-    // users can see that the simulator is alive and well.
-    PW_LOG_INFO("Simulated device is still alive");
-    // TODO(amontanez): This thread should probably have a way to exit.
-  }
-}
