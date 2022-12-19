03de09bc66ad38ad4c17919c9f8b008a4f3a1e91
Marie Janssen <jamuraa@google.com>
2022-12-19 16:47:34 +0000

    [bt][common] Add WeakSelf replacing WeakPtrFactory
    Introduce the WeakSelf template, which replaces use of
    fxl::WeakPtrFactory patterns.  Usage:
    
    class A: WeakSelf<A> {
      A() : WeakSelf(this) {}
    
      void func() {
        auto cb = [weak = GetWeakPtr()]() {
          if (weak.is_alive()) {
            weak->InteractWith();
          }
        }
        // cb can be moved to the async loop, assigned as a callback, etc.
      }
    }
    
    Bug: BT-2243
    Bug: 2766
    Bug: 894
    Test: Added unit tests; fx test bt-host-common-tests
    
    Change-Id: I9bca988dd0ab916f472f85ac0e551bc8118593ba
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/305790
    Commit-Queue: Marie Janssen <jamuraa@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

---
+++ pw_bluetooth_sapphire/host/common/weak_self.h
@@ -0,0 +1,224 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_WEAK_SELF_H_
+#define SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_WEAK_SELF_H_
+
+#include <optional>
+
+#include <fbl/recycler.h>
+#include <fbl/ref_counted.h>
+#include <fbl/ref_ptr.h>
+
+#include "src/connectivity/bluetooth/core/bt-host/common/assert.h"
+
+template <typename T>
+class DynamicWeakManager;
+
+// WeakRef is an intrusively-counted reference to an object that may or may not still exist.
+// Check is_alive() before using the get() function to get a reference to
+// the object.
+//
+// This is not thread-safe: get() must be used on the thread the WeakPtr was created on
+// (but can be passed through other threads while not being used)
+template <typename T>
+class WeakRef : public fbl::RefCounted<WeakRef<T>> {
+ public:
+  ~WeakRef() = default;
+
+  // Returns true if the object referred is alive.
+  // If this returns true, WeakRef<T>::Get() will work.
+  bool is_alive() { return !!ptr_; }
+
+  // Get a reference to the alive object.
+  T& get() {
+    BT_ASSERT_MSG(ptr_, "attempted to get a destroyed ptr");
+    return *ptr_;
+  }
+
+  void set(T* p) { ptr_ = p; }
+
+ private:
+  explicit WeakRef(T* ptr) : ptr_(ptr) {}
+
+  void maybe_unset(const T* doomed) {
+    if (ptr_ == doomed) {
+      ptr_ = nullptr;
+    }
+  }
+  // Pointer to the existent object if it is alive, otherwise a nullptr.
+  T* ptr_;
+  friend class DynamicWeakManager<T>;
+};
+
+// RecyclingWeakRef is a version of WeakRef which avoids deletion after the last count is
+// destructed, instead marking itself as not in use, for reuse by a WeakManager that
+// maintains a pool of RecyclingWeakRefs for static memory usage.
+//
+// For an example, see OnlyTwoStaticManager in the unit tests for WeakSelf.
+//
+// We disable the Adoption Validator of RefCounted so that this pointer can be adopted multiple
+// times. Otherwise RefCountedBase gets mad as us for not resetting the count to the sentinel (which
+// is not possible).
+template <typename T>
+class RecyclingWeakRef
+    : public fbl::Recyclable<RecyclingWeakRef<T>>,
+      public fbl::RefCounted<RecyclingWeakRef<T>, /*EnableAdoptionValidator=*/false> {
+ public:
+  RecyclingWeakRef() : ptr_(nullptr) {}
+  ~RecyclingWeakRef() = default;
+
+  // Returns true if the object referred is alive.
+  // If this returns true, Get() will work.
+  // If this returns true, in_use will also return true.
+  bool is_alive() { return !!ptr_; }
+
+  // Returns true if this ref is in use.
+  // This can return true while is_alive returns false.
+  bool is_in_use() { return in_use_; }
+
+  T& get() {
+    BT_ASSERT_MSG(in_use_, "shouldn't get an unallocated ptr");
+    BT_ASSERT_MSG(ptr_, "attempted to get a destroyed ptr");
+    return *ptr_;
+  }
+
+  fbl::RefPtr<RecyclingWeakRef<T>> alloc(T* p) {
+    BT_ASSERT(!in_use_);
+    in_use_ = true;
+    ptr_ = p;
+    return fbl::AdoptRef(this);
+  }
+
+  void maybe_unset(const T* doomed) {
+    if (in_use_ && ptr_ == doomed) {
+      ptr_ = nullptr;
+    }
+  }
+
+ private:
+  friend class fbl::Recyclable<RecyclingWeakRef<T>>;
+  // This method is called by Recyclable on the last reference drop.
+  void fbl_recycle() {
+    ptr_ = nullptr;
+    in_use_ = false;
+  }
+
+  // True if this pointer is in use.
+  bool in_use_ = false;
+
+  // Pointer to the existent object if it is alive, otherwise a nullptr.
+  T* ptr_;
+};
+
+// Default Manager for Weak Pointers. Each object that derives from WeakSelf holds one manager
+// object. This indirection is used to enable shared static memory weak pointers across multiple
+// copies of the same class of objects.
+//
+// The default manager allocates a single weak pointer for each object that acquires at least one
+// weak reference, and holds the weak reference alive until the object referenced is destroyed.
+template <typename T>
+class DynamicWeakManager {
+ public:
+  explicit DynamicWeakManager(T* self_ptr) : self_ptr_(self_ptr), weak_ptr_ref_(nullptr) {}
+
+  using RefType = WeakRef<T>;
+
+  ~DynamicWeakManager() {
+    if (weak_ptr_ref_) {
+      weak_ptr_ref_->maybe_unset(self_ptr_);
+    }
+  }
+
+  std::optional<fbl::RefPtr<RefType>> GetWeakRef() {
+    if (weak_ptr_ref_ == nullptr) {
+      weak_ptr_ref_ = fbl::AdoptRef(new RefType(self_ptr_));
+    }
+    return weak_ptr_ref_;
+  }
+
+ private:
+  T* self_ptr_;
+  fbl::RefPtr<WeakRef<T>> weak_ptr_ref_;
+};
+
+// WeakSelf is a class used to create pointers to an object that must be checked before
+// using - because their target may have been destroyed.  These are termed "weak pointers"
+// and can be vended in one of two ways:
+// (1) inheriting from the WeakSelf class and initializing it on construction,
+//
+// class A: WeakSelf<A> {
+//    A() : WeakSelf(this) {}
+//    auto MakeSelfReferentialCallback() {
+//       auto cb = [weak = GetWeakPtr()] {
+//          if (weak.is_alive()) {
+//             weak->AnotherFunction();
+//          }
+//       }
+//       return std::move(cb);
+//     }
+//  };
+//
+// (2) making a WeakSelf<T> a member of your class and using it to vend pointers.
+//
+// class A {
+//  public:
+//    A() : weak_factory_(this) {}
+//    auto MakeSelfReferentialCallback() {
+//       auto cb = [weak = weak_factory_.GetWeakPtr()] {
+//          if (weak.is_alive()) {
+//             weak->AnotherFunction();
+//          }
+//       }
+//       return std::move(cb);
+//     }
+//   private:
+//    // Other members should be defined before weak_factory_ so it is destroyed first.
+//    WeakSelf<A> weak_factory_;
+//  };
+//
+// The first method is preferable if you expect to vend weak pointers outside the class, as the
+// WeakSelf::GetWeakPtr function is public. However, note that with the first method, members of the
+// class will be destroyed before the class is destroyed - it may be undesirable if during
+// destruction, the weak pointer should be considered dead.
+template <typename T, typename WeakPtrManager = DynamicWeakManager<T>>
+class WeakSelf {
+ public:
+  explicit WeakSelf(T* self_ptr) : manager_(self_ptr) {}
+  ~WeakSelf() = default;
+
+  class WeakPtr {
+   public:
+    // Default-constructed WeakPtrs point nowhere and aren't alive.
+    WeakPtr() : ptr_(nullptr) {}
+    explicit WeakPtr(std::nullptr_t) : WeakPtr() {}
+
+    bool is_alive() const { return ptr_ && ptr_->is_alive(); }
+    T& get() const {
+      BT_ASSERT_MSG(ptr_, "tried to get never-assigned weak pointer");
+      return ptr_->get();
+    }
+    T* operator->() const { return &get(); }
+
+    void reset() { ptr_ = nullptr; }
+
+   private:
+    explicit WeakPtr(fbl::RefPtr<typename WeakPtrManager::RefType> ptr) : ptr_(ptr) {}
+
+    fbl::RefPtr<typename WeakPtrManager::RefType> ptr_;
+
+    friend WeakSelf<T, WeakPtrManager>;
+  };
+
+  WeakPtr GetWeakPtr() {
+    auto weak_ref = manager_.GetWeakRef();
+    BT_ASSERT_MSG(weak_ref.has_value(), "weak pointer not available");
+    return WeakPtr(*weak_ref);
+  }
+
+ private:
+  WeakPtrManager manager_;
+};
+
+#endif  // SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_WEAK_SELF_H_
---
+++ pw_bluetooth_sapphire/host/common/weak_self_unittest.cc
@@ -0,0 +1,176 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "weak_self.h"
+
+#include <lib/async/cpp/task.h>
+#include <lib/async/default.h>
+#include <lib/async/dispatcher.h>
+
+#include <gtest/gtest.h>
+
+#include "src/lib/testing/loop_fixture/test_loop_fixture.h"
+
+namespace bt {
+namespace {
+
+using WeakSelfTest = ::gtest::TestLoopFixture;
+
+class FunctionTester : public WeakSelf<FunctionTester> {
+ public:
+  explicit FunctionTester(uint8_t testval) : WeakSelf(this), value_(testval) {}
+
+  void callback_later_with_weak(fit::function<void(FunctionTester::WeakPtr)> cb) {
+    auto weak = GetWeakPtr();
+    auto timed = [self = std::move(weak), cb = std::move(cb)]() { cb(self); };
+    async::PostTask(async_get_default_dispatcher(), std::move(timed));
+  }
+
+  uint8_t value() const { return value_; }
+
+ private:
+  uint8_t value_;
+};
+
+TEST_F(WeakSelfTest, InvalidatingSelf) {
+  bool called = false;
+  FunctionTester::WeakPtr ptr;
+
+  // Default-constructed weak pointers are not alive.
+  EXPECT_FALSE(ptr.is_alive());
+
+  auto cb = [&ptr, &called](auto weakptr) {
+    called = true;
+    ptr = weakptr;
+  };
+
+  {
+    FunctionTester test(0xBA);
+
+    test.callback_later_with_weak(cb);
+
+    // Run the loop until we're called back.
+    RunLoopUntilIdle();
+
+    EXPECT_TRUE(called);
+    EXPECT_TRUE(ptr.is_alive());
+    EXPECT_EQ(&test, &ptr.get());
+    EXPECT_EQ(0xBA, ptr->value());
+
+    called = false;
+    test.callback_later_with_weak(cb);
+
+    // Now out of scope.
+  }
+
+  // Run the loop until we're called back.
+  RunLoopUntilIdle();
+
+  EXPECT_TRUE(called);
+  EXPECT_FALSE(ptr.is_alive());
+  EXPECT_DEATH_IF_SUPPORTED(ptr.get(), "destroyed");
+}
+
+class StaticTester;
+
+class OnlyTwoStaticManager {
+ public:
+  explicit OnlyTwoStaticManager(StaticTester *self_ptr) : obj_ptr_(self_ptr) {}
+  ~OnlyTwoStaticManager() {
+    OnlyTwoStaticManager::pointers_[0].maybe_unset(obj_ptr_);
+    OnlyTwoStaticManager::pointers_[1].maybe_unset(obj_ptr_);
+  }
+
+  using RefType = RecyclingWeakRef<StaticTester>;
+
+  std::optional<fbl::RefPtr<RefType>> GetWeakRef() {
+    for (auto &ptr : OnlyTwoStaticManager::pointers_) {
+      if (ptr.is_alive() && &ptr.get() == obj_ptr_) {
+        // Already adopted, add another refptr pointing to it.
+        return fbl::RefPtr(&ptr);
+      }
+    }
+    for (auto &ptr : OnlyTwoStaticManager::pointers_) {
+      if (!ptr.is_in_use()) {
+        return ptr.alloc(obj_ptr_);
+      }
+    }
+    return std::nullopt;
+  }
+
+ private:
+  StaticTester *obj_ptr_;
+  inline static RecyclingWeakRef<StaticTester> pointers_[2];
+};
+
+class StaticTester : public WeakSelf<StaticTester, OnlyTwoStaticManager> {
+ public:
+  explicit StaticTester(uint8_t testval) : WeakSelf(this), value_(testval) {}
+
+  uint8_t value() const { return value_; }
+
+ private:
+  uint8_t value_;
+};
+
+TEST_F(WeakSelfTest, StaticRecyclingPointers) {
+  // We can create more objects than we have weak space for.
+  StaticTester test1(1);
+  StaticTester test2(2);
+  StaticTester test3(3);
+
+  // And create as many weak pointers of one of them as we want.
+  auto ptr = test1.GetWeakPtr();
+  auto ptr2 = test1.GetWeakPtr();
+  auto ptr3 = test1.GetWeakPtr();
+  auto ptr4 = ptr;
+
+  // Make the second one have some ptrs too.
+  {
+    {
+      StaticTester test4(4);
+      auto second_ptr = test4.GetWeakPtr();
+      auto second_ptr2 = test4.GetWeakPtr();
+      EXPECT_TRUE(ptr4.is_alive());
+      StaticTester *ptr4_old = &ptr4.get();
+      ptr4 = second_ptr;
+      EXPECT_TRUE(ptr4.is_alive());
+      // It's moved to the new one though.
+      EXPECT_NE(&ptr4.get(), ptr4_old);
+      EXPECT_EQ(&ptr4.get(), &test4);
+    }
+    // ptr4 outlived it's target.
+    EXPECT_FALSE(ptr4.is_alive());
+    // Now let's make the second weak pointer unused, recycling it.
+    ptr4 = ptr3;
+  }
+
+  // Now I can get a second weak ptr still, from our third object.
+  auto still_okay = test3.GetWeakPtr();
+  auto still_copy = still_okay;
+  EXPECT_TRUE(still_copy.is_alive());
+}
+
+TEST_F(WeakSelfTest, StaticDeathWhenExhausted) {
+  StaticTester test1(1);
+  StaticTester test3(3);
+
+  auto ptr1 = test1.GetWeakPtr();
+  auto ptr2 = ptr1;
+  {
+    StaticTester test2(2);
+
+    ptr2 = test2.GetWeakPtr();
+
+    EXPECT_TRUE(ptr2.is_alive());
+    EXPECT_TRUE(ptr1.is_alive());
+  }
+
+  EXPECT_FALSE(ptr2.is_alive());
+
+  EXPECT_DEATH_IF_SUPPORTED(test3.GetWeakPtr(), ".*");
+}
+
+}  // namespace
+}  // namespace bt
