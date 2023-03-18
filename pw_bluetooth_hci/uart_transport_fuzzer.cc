8ab5125b880df9119fae43a8068165781c0792e6
Aaron Green <aarongreen@google.com>
2023-03-18 00:59:08 +0000

    pw_bluetooth_hci: Fix uart_transport_fuzzer
    This CL fixes the fuzzing code which bit rot when the fuzzer tests were
    disabled.
    
    Change-Id: I8e15a8bc683491028009c056aed45363c102423a
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/131592
    Reviewed-by: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Ewout van Bekkum <ewout@google.com>
    Commit-Queue: Aaron Green <aarongreen@google.com>

--- pw_bluetooth_hci/uart_transport_fuzzer.cc
+++ pw_bluetooth_hci/uart_transport_fuzzer.cc
@@ -36,16 +36,18 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         const CommandPacket& command_packet = packet.command_packet();
 
         const uint16_t opcode = command_packet.opcode();
-        stream.Write(as_bytes(span<const uint16_t>(&opcode, 1)));
+        stream.Write(as_bytes(span<const uint16_t>(&opcode, 1))).IgnoreError();
 
         const uint16_t opcode_command_field =
             command_packet.opcode_command_field();
-        stream.Write(as_bytes(span<const uint16_t>(&opcode_command_field, 1)));
+        stream.Write(as_bytes(span<const uint16_t>(&opcode_command_field, 1)))
+            .IgnoreError();
 
         const uint8_t opcode_group_field = command_packet.opcode_group_field();
-        stream.Write(as_bytes(span<const uint8_t>(&opcode_group_field, 1)));
+        stream.Write(as_bytes(span<const uint8_t>(&opcode_group_field, 1)))
+            .IgnoreError();
 
-        stream.Write(command_packet.parameters());
+        stream.Write(command_packet.parameters()).IgnoreError();
         return;
       }
 
@@ -54,19 +56,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
         const uint16_t handle_and_fragmentation_bits =
             async_data_packet.handle_and_fragmentation_bits();
-        stream.Write(
-            as_bytes(span<const uint16_t>(&handle_and_fragmentation_bits, 1)));
+        stream
+            .Write(as_bytes(
+                span<const uint16_t>(&handle_and_fragmentation_bits, 1)))
+            .IgnoreError();
 
         const uint16_t handle = async_data_packet.handle();
-        stream.Write(as_bytes(span<const uint16_t>(&handle, 1)));
+        stream.Write(as_bytes(span<const uint16_t>(&handle, 1))).IgnoreError();
 
         const uint8_t pb_flag = async_data_packet.pb_flag();
-        stream.Write(as_bytes(span<const uint8_t>(&pb_flag, 1)));
+        stream.Write(as_bytes(span<const uint8_t>(&pb_flag, 1))).IgnoreError();
 
         const uint8_t bc_flag = async_data_packet.bc_flag();
-        stream.Write(as_bytes(span<const uint8_t>(&bc_flag, 1)));
+        stream.Write(as_bytes(span<const uint8_t>(&bc_flag, 1))).IgnoreError();
 
-        stream.Write(async_data_packet.data());
+        stream.Write(async_data_packet.data()).IgnoreError();
         return;
       }
 
@@ -75,17 +79,18 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
         const uint16_t handle_and_status_bits =
             sync_data_packet.handle_and_status_bits();
-        stream.Write(
-            as_bytes(span<const uint16_t>(&handle_and_status_bits, 1)));
+        stream.Write(as_bytes(span<const uint16_t>(&handle_and_status_bits, 1)))
+            .IgnoreError();
 
         const uint16_t handle = sync_data_packet.handle();
-        stream.Write(as_bytes(span<const uint16_t>(&handle, 1)));
+        stream.Write(as_bytes(span<const uint16_t>(&handle, 1))).IgnoreError();
 
         const uint8_t packet_status_flag =
             sync_data_packet.packet_status_flag();
-        stream.Write(as_bytes(span<const uint8_t>(&packet_status_flag, 1)));
+        stream.Write(as_bytes(span<const uint8_t>(&packet_status_flag, 1)))
+            .IgnoreError();
 
-        stream.Write(sync_data_packet.data());
+        stream.Write(sync_data_packet.data()).IgnoreError();
         return;
       }
 
@@ -93,9 +98,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         const EventPacket& event_packet = packet.event_packet();
 
         const uint8_t event_code = event_packet.event_code();
-        stream.Write(as_bytes(span<const uint8_t>(&event_code, 1)));
+        stream.Write(as_bytes(span<const uint8_t>(&event_code, 1)))
+            .IgnoreError();
 
-        stream.Write(event_packet.parameters());
+        stream.Write(event_packet.parameters()).IgnoreError();
         return;
       }
 
