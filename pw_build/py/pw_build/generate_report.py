56cb3bba0794de2526b650da1bdc6b9332505ec6
Greg Pataky <gregpataky@google.com>
2023-04-20 22:17:25 +0000

    pw_build: Update coverage_report.gni to support new -ffile-prefix-map
    Shortly before the original coverage_report.gni CL landed, a CL went out
    that updated the `-ffile-prefix-map` mappings. This broke the way we
    were doing coverage report generation since we were relying on the old
    file path mappings and the fact that `llvm-cov` would prepend the CWD to
    the path before trying to resolve it (in this case, that was always the
    build directory).
    
    This CL updates to specify a `--compilation-dir` mapping to the root
    project directory. To handle us mapping the build directory to `out`
    always, we then use the `--path-equivalence` option to map that to the
    current build directory so that the coverage report can correctly find
    the files.
    
    Change-Id: I191abea0bd1f34d2b1eeec8bc57f3f4eb86ffce2
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/138470
    Reviewed-by: Gulfem Savrun Yeniceri <gulfem@google.com>
    Reviewed-by: Carlos Chinchilla <cachinchilla@google.com>
    Commit-Queue: Greg Pataky <gregpataky@google.com>
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- pw_build/py/pw_build/generate_report.py
+++ pw_build/py/pw_build/generate_report.py
@@ -162,14 +162,31 @@ def generate_report(
             # `text` is JSON format when using `llvm-cov`.
             format_type if format_type == 'lcov' else 'text',
         ]
+    # We really need two `--path-equivalence` options to be able to map both the
+    # root directory for coverage files to the absolute path of the project
+    # root_dir and to be able to map "out/" prefix to the provided build_dir.
+    #
+    # llvm-cov does not currently support two `--path-equivalence` options, so
+    # we use `--compilation-dir` and `--path-equivalence` together. This has the
+    # unfortunate consequence of showing file paths as absolute in the JSON,
+    # LCOV, and text reports.
+    #
     # An unwritten assumption here is that root_dir must be an
     # absolute path to enable file-path-based filtering.
     #
     # This is due to turning all file paths into absolute files here:
     # https://github.com/llvm-mirror/llvm/blob/2c4ca6832fa6b306ee6a7010bfb80a3f2596f824/tools/llvm-cov/CodeCoverage.cpp#L188.
     command += [
+        '--compilation-dir',
+        str(root_dir),
+    ]
+    # Pigweed maps any build directory to out, which causes generated files to
+    # be reported to exist under the out directory, which may not exist if the
+    # build directory is not exactly out. This maps out back to the build
+    # directory so generated files can be found.
+    command += [
         '--path-equivalence',
-        f'{build_dir.relative_to(root_dir)},{root_dir}',
+        f'{str(root_dir)}/out,{str(build_dir)}',
     ]
     command += [
         '--instr-profile',
@@ -198,7 +215,7 @@ def generate_report(
     else:  # format_type in ['lcov', 'json']
         output = subprocess.run(command, capture_output=True)
         if output.returncode != 0:
-            print(output.stderr, file=sys.stderr)
+            _LOG.error(output.stderr)
             return output.returncode
         export_output_path.write_bytes(output.stdout)
 
