30fea99cf13859dea202831a22c6b7d4e2b9f41e
Aaron Green <aarongreen@google.com>
2023-04-26 20:48:51 +0000

    pw_build: Add GnTarget
    This CL adds a Python class to generate and abstract GN targets
    such as `pw_source_set`s and `pw_executable`s from Bazel rules or JSON
    objects.
    
    Change-Id: I231bd08e4ca213b84f8d69a70066999528ed03e4
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/138701
    Commit-Queue: Aaron Green <aarongreen@google.com>
    Reviewed-by: Chad Norvell <chadnorvell@google.com>

---
+++ pw_build/py/gn_target_test.py
@@ -0,0 +1,211 @@
+# Copyright 2023 The Pigweed Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Tests for the pw_build.gn_target module."""
+
+import unittest
+
+from pw_build.bazel_query import BazelRule
+from pw_build.gn_target import GnTarget
+from pw_build.gn_utils import GnLabel, MalformedGnError
+
+
+class TestGnTarget(unittest.TestCase):
+    """Tests for gn_target.GnTarget."""
+
+    def setUp(self):
+        self.rule = BazelRule('//my-package:my-target', 'cc_library')
+
+    def test_from_bazel_rule_type_source_set(self):
+        """Tests creating a `pw_source_set` from a Bazel rule."""
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(target.type(), 'pw_source_set')
+
+    def test_from_bazel_rule_type_static_lib(self):
+        """Tests creating a `pw_static_library` from a Bazel rule."""
+        self.rule.set_attr('linkstatic', True)
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(target.type(), 'pw_static_library')
+
+    def test_from_bazel_rule_type_invalid(self):
+        """Tests creating an invalid type from a Bazel rule."""
+        with self.assertRaises(MalformedGnError):
+            rule = BazelRule('//my-package:my-target', 'custom_type')
+            GnTarget('$build', '$src', bazel=rule)
+
+    def test_from_bazel_rule_visibility(self):
+        """Tests getting the GN visibility from a Bazel rule."""
+        self.rule.add_visibility('//visibility:private')
+        self.rule.add_visibility('//foo:__subpackages__')
+        self.rule.add_visibility('//foo/bar:__pkg__')
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            {str(scope) for scope in target.visibility},
+            {
+                '$build/my-package:*',
+                '$build/foo/*',
+                '$build/foo/bar:*',
+            },
+        )
+
+    def test_from_bazel_rule_visibility_public(self):
+        """Tests that 'public' overrides any other visibility"""
+        self.rule.add_visibility('//visibility:private')
+        self.rule.add_visibility('//visibility:public')
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(target.visibility, [])
+
+    def test_from_bazel_rule_visibility_invalid(self):
+        """Tests that and invalid visibility raises an error."""
+        self.rule.add_visibility('invalid')
+        with self.assertRaises(MalformedGnError):
+            GnTarget('$build', '$src', bazel=self.rule)
+
+    def test_from_bazel_rule_testonly_unset(self):
+        """Tests that omitting `testonly` defaults it to False."""
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertFalse(target.testonly)
+
+    def test_from_bazel_rule_testonly_false(self):
+        """Tests setting `testonly` to False."""
+        self.rule.set_attr('testonly', False)
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertFalse(target.testonly)
+
+    def test_from_bazel_rule_testonly_true(self):
+        """Tests setting `testonly` to True."""
+        self.rule.set_attr('testonly', True)
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertTrue(target.testonly)
+
+    def test_from_bazel_rule_public(self):
+        """Tests getting the GN public headers from a Bazel rule."""
+        self.rule.set_attr('hdrs', ['//foo:bar.h', '//:baz.h'])
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            {str(path) for path in target.public},
+            {
+                '$src/foo/bar.h',
+                '$src/baz.h',
+            },
+        )
+
+    def test_from_bazel_rule_sources(self):
+        """Tests getting the GN source files from a Bazel rule."""
+        self.rule.set_attr('srcs', ['//foo:bar.cc', '//:baz.cc'])
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            {str(path) for path in target.sources},
+            {
+                '$src/foo/bar.cc',
+                '$src/baz.cc',
+            },
+        )
+
+    def test_from_bazel_rule_inputs(self):
+        """Tests getting the GN input files from a Bazel rule."""
+        self.rule.set_attr(
+            'additional_linker_inputs', ['//foo:bar.data', '//:baz.data']
+        )
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            {str(path) for path in target.inputs},
+            {
+                '$src/foo/bar.data',
+                '$src/baz.data',
+            },
+        )
+
+    def test_from_bazel_rule_include_dirs(self):
+        """Tests getting the GN include directories from a Bazel rule."""
+        self.rule.set_attr('includes', ['//foo'])
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            set(target.config.get('include_dirs')),
+            {
+                '$src',
+                '$src/foo',
+            },
+        )
+
+    def test_from_bazel_rule_configs(self):
+        """Tests getting the GN config flags from a Bazel rule."""
+        self.rule.set_attr('defines', ['KEY1=VAL1'])
+        self.rule.set_attr('copts', ['-frobinator'])
+        self.rule.set_attr('linkopts', ['-fizzbuzzer'])
+        self.rule.set_attr('local_defines', ['KEY2=VAL2', 'KEY3=VAL3'])
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            set(target.config.get('public_defines')), {'KEY1=VAL1'}
+        )
+        self.assertEqual(set(target.config.get('cflags')), {'-frobinator'})
+        self.assertEqual(set(target.config.get('ldflags')), {'-fizzbuzzer'})
+        self.assertEqual(
+            set(target.config.get('defines')), {'KEY2=VAL2', 'KEY3=VAL3'}
+        )
+
+    def test_from_bazel_rule_deps(self):
+        """Tests getting the GN dependencies from a Bazel rule."""
+        self.rule.set_attr(
+            'deps', ['//my-package:foo', '@com_corp_project//bar']
+        )
+        self.rule.set_attr(
+            'implementation_deps',
+            [
+                '//other-pkg/subdir',
+                '@com_corp_project//:top-level',
+            ],
+        )
+        target = GnTarget('$build', '$src', bazel=self.rule)
+        self.assertEqual(
+            {str(label) for label in target.public_deps},
+            {
+                '$build/my-package:foo',
+                '$repo/bar',
+            },
+        )
+        self.assertEqual(
+            {str(label) for label in target.deps},
+            {
+                '$build/other-pkg/subdir',
+                '$repo:top-level',
+            },
+        )
+
+    def test_make_relative_adjacent(self):
+        """Tests rebasing labels for a target."""
+        target = GnTarget('$build/pkg', '$src')
+        label = target.make_relative(GnLabel('$build/pkg:adjacent-config'))
+        self.assertEqual(label, ":adjacent-config")
+
+    def test_make_relative_absolute(self):
+        """Tests rebasing labels for a target."""
+        target = GnTarget('$build/pkg', '$src')
+        label = target.make_relative(GnLabel('//absolute:config'))
+        self.assertEqual(label, "//absolute:config")
+
+    def test_make_relative_descend(self):
+        """Tests rebasing labels for a target."""
+        target = GnTarget('$build/pkg', '$src')
+        label = target.make_relative(GnLabel('$build/pkg/relative:dep'))
+        self.assertEqual(label, "relative:dep")
+
+    def test_make_relative_ascend(self):
+        """Tests rebasing labels for a target."""
+        target = GnTarget('$build/pkg', '$src')
+        label = target.make_relative(GnLabel('$build/dotdot/relative'))
+        self.assertEqual(label, "../dotdot/relative")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pw_build/py/pw_build/gn_target.py
@@ -0,0 +1,308 @@
+# Copyright 2023 The Pigweed Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Utilities for manipulating GN targets."""
+
+from json import loads as json_loads, dumps as json_dumps
+from pathlib import PurePosixPath
+from typing import Dict, List, Optional, Set, Union
+
+from pw_build.bazel_query import BazelRule
+from pw_build.gn_config import GnConfig, GN_CONFIG_FLAGS
+from pw_build.gn_utils import (
+    GnLabel,
+    GnPath,
+    GnVisibility,
+    MalformedGnError,
+)
+
+
+class GnTarget:  # pylint: disable=too-many-instance-attributes
+    """Represents a Pigweed Build target.
+
+    Attributes:
+        config: Variables on the target that can be added to a config, e.g.
+            those in `pw_build.gn_config.GN_CONFIG_FLAGS.
+
+    The following attributes match those of `pw_internal_build_target` in
+    //pw_build/gn_internal/build_target.gni
+        remove_configs
+
+    The following attributes are analogous to GN variables, see
+    https://gn.googlesource.com/gn/+/main/docs/reference.md#target_variables:
+        visibility
+        testonly
+        public
+        sources
+        inputs
+        public_configs
+        configs
+        public_deps
+        deps
+    """
+
+    def __init__(
+        self,
+        base_label: Union[str, PurePosixPath, GnLabel],
+        base_path: Union[str, PurePosixPath, GnPath],
+        bazel: Optional[BazelRule] = None,
+        json: Optional[str] = None,
+    ) -> None:
+        """Creates a GN target
+
+        Args:
+
+        """
+        self._type: str
+        self._label: GnLabel
+        self._base_label: GnLabel = GnLabel(base_label)
+        self._base_path: GnPath = GnPath(base_path)
+        self._repos: Set[str] = set()
+        self.visibility: List[GnVisibility] = []
+        self.testonly: bool = False
+        self.public: List[GnPath] = []
+        self.sources: List[GnPath] = []
+        self.inputs: List[GnPath] = []
+        self.config: GnConfig = GnConfig()
+        self.public_configs: Set[GnLabel] = set()
+        self.configs: Set[GnLabel] = set()
+        self.remove_configs: Set[GnLabel] = set()
+        self.public_deps: Set[GnLabel] = set()
+        self.deps: Set[GnLabel] = set()
+        if bazel:
+            self._from_bazel(bazel)
+        elif json:
+            self._from_json(json)
+        else:
+            self._label = GnLabel(base_label)
+
+    def _from_bazel(self, rule: BazelRule) -> None:
+        """Populates target info from a Bazel Rule.
+
+        Filenames will be relative to the given path to the source directory.
+
+        Args:
+            rule: The Bazel rule to populate this object with.
+        """
+        kind = rule.kind()
+        linkstatic = rule.get_bool('linkstatic')
+        visibility = set(rule.get_visibility())
+
+        self.testonly = rule.get_bool('testonly')
+        public = rule.get_list('hdrs')
+        sources = rule.get_list('srcs')
+        inputs = rule.get_list('additional_linker_inputs')
+
+        include_dirs = rule.get_list('includes')
+        self.config.add('public_defines', *rule.get_list('defines'))
+        self.config.add('cflags', *rule.get_list('copts'))
+        self.config.add('ldflags', *rule.get_list('linkopts'))
+        self.config.add('defines', *rule.get_list('local_defines'))
+
+        public_deps = rule.get_list('deps')
+        deps = rule.get_list('implementation_deps')
+
+        rule_label = rule.label()
+        if rule_label.startswith('//'):
+            rule_label = rule_label[2:]
+        self._label = GnLabel(self._base_label.joinlabel(rule_label))
+
+        # Translate Bazel kind to GN target type.
+        if kind == 'cc_library':
+            if linkstatic:
+                self._type = 'pw_static_library'
+            else:
+                self._type = 'pw_source_set'
+        else:
+            raise MalformedGnError(f'unsupported Bazel kind: {kind}')
+
+        # Bazel treats no `visibility` (and no package default) as private,
+        # whereas GN treats no `visibility` as public.
+        visibility.add('//visibility:private')
+        for scope in visibility:
+            self.add_visibility(bazel=scope)
+        if any(str(scope) == '//*' for scope in self.visibility):
+            self.visibility = []
+
+        # Add includer directories. Bazel implicitly includes the project root.
+        include_dirs.append('//')
+        gn_paths = [
+            GnPath(self._base_path, bazel=file) for file in include_dirs
+        ]
+        self.config.add('include_dirs', *[str(gn_path) for gn_path in gn_paths])
+
+        for path in public:
+            self.add_path('public', bazel=path)
+        for path in sources:
+            self.add_path('sources', bazel=path)
+        for path in inputs:
+            self.add_path('inputs', bazel=path)
+        for label in public_deps:
+            self.add_dep(public=True, bazel=label)
+        for label in deps:
+            self.add_dep(bazel=label)
+
+    def _from_json(self, data: str) -> None:
+        """Populates this target from a JSON string.
+
+        Args:
+            data: The JSON data to populate this object with.
+        """
+        obj = json_loads(data)
+        self._type = obj.get('target_type')
+        name = obj.get('target_name')
+        package = obj.get('package', '')
+        self._label = GnLabel(
+            self._base_label.joinlabel(package), gn=f':{name}'
+        )
+        self.visibility = [
+            GnVisibility(self._base_label, self._label, gn=scope)
+            for scope in obj.get('visibility', [])
+        ]
+        self.testonly = bool(obj.get('testonly', False))
+        self.public = [GnPath(path) for path in obj.get('public', [])]
+        self.sources = [GnPath(path) for path in obj.get('sources', [])]
+        self.inputs = [GnPath(path) for path in obj.get('inputs', [])]
+        for flag in GN_CONFIG_FLAGS:
+            if flag in obj:
+                self.config.add(flag, *obj[flag])
+        self.public_configs = {
+            GnLabel(label) for label in obj.get('public_configs', [])
+        }
+        self.configs = {GnLabel(label) for label in obj.get('configs', [])}
+        self.public_deps = {
+            GnLabel(label) for label in obj.get('public_deps', [])
+        }
+        self.deps = {GnLabel(label) for label in obj.get('deps', [])}
+
+    def to_json(self) -> str:
+        """Returns a JSON representation of this target."""
+        obj: Dict[str, Union[bool, str, List[str]]] = {}
+        if self._type:
+            obj['target_type'] = self._type
+        obj['target_name'] = self.name()
+        obj['package'] = self.package()
+        if self.visibility:
+            obj['visibility'] = [str(scope) for scope in self.visibility]
+        if self.testonly:
+            obj['testonly'] = self.testonly
+        if self.public:
+            obj['public'] = [str(path) for path in self.public]
+        if self.sources:
+            obj['sources'] = [str(path) for path in self.sources]
+        if self.inputs:
+            obj['inputs'] = [str(path) for path in self.inputs]
+        for flag in GN_CONFIG_FLAGS:
+            if self.config.has(flag):
+                obj[flag] = list(self.config.get(flag))
+        if self.public_configs:
+            obj['public_configs'] = [
+                str(label) for label in self.public_configs
+            ]
+        if self.configs:
+            obj['configs'] = [str(label) for label in self.configs]
+        if self.remove_configs:
+            obj['remove_configs'] = [
+                str(label) for label in self.remove_configs
+            ]
+        if self.public_deps:
+            obj['public_deps'] = [str(label) for label in self.public_deps]
+        if self.deps:
+            obj['deps'] = [str(label) for label in self.deps]
+        return json_dumps(obj)
+
+    def name(self) -> str:
+        """Returns the target name."""
+        return self._label.name()
+
+    def type(self) -> str:
+        """Returns the target type."""
+        return self._type
+
+    def repos(self) -> Set[str]:
+        """Returns any external repositories referenced from Bazel rules."""
+        return self._repos
+
+    def package(self) -> str:
+        """Returns the relative path from the base label."""
+        pkgname = self._label.relative_to(self._base_label).split(':')[0]
+        return '' if pkgname == '.' else pkgname
+
+    def add_path(self, dst: str, **kwargs) -> None:
+        """Adds a GN path using this target's source root.
+
+        Args:
+            dst: Variable to add the path to. Should be one of 'public',
+                'sources', or 'inputs'.
+
+        Keyword Args:
+            Same as `GnPath.__init__`.
+        """
+        getattr(self, dst).append(GnPath(self._base_path, **kwargs))
+
+    def add_config(self, label: GnLabel, public: bool = False) -> None:
+        """Adds a GN config to this target.
+
+        Args:
+            label: The label of the config to add to this target.
+            public: If true, the config is added as a `public_config`.
+        """
+        self.remove_configs.discard(label)
+        if public:
+            self.public_configs.add(label)
+            self.configs.discard(label)
+        else:
+            self.public_configs.discard(label)
+            self.configs.add(label)
+
+    def remove_config(self, label: GnLabel) -> None:
+        """Adds the given config to the list of default configs to remove.
+
+        Args:
+            label: The config to add to `remove_configs`.
+        """
+        self.public_configs.discard(label)
+        self.configs.discard(label)
+        self.remove_configs.add(label)
+
+    def add_dep(self, **kwargs) -> None:
+        """Adds a GN dependency to the target using this target's base label.
+
+        Keyword Args:
+            Same as `GnLabel.__init__`.
+        """
+        dep = GnLabel(self._base_label, **kwargs)
+        repo = dep.repo()
+        if repo:
+            self._repos.add(repo)
+        if dep.public():
+            self.public_deps.add(dep)
+        else:
+            self.deps.add(dep)
+
+    def add_visibility(self, **kwargs) -> None:
+        """Adds a GN visibility scope using this target's base label.
+
+        Keyword Args:
+            Same as `GnVisibility.__init__`.
+        """
+        visibility = GnVisibility(self._base_label, self._label, **kwargs)
+        self.visibility.append(visibility)
+
+    def make_relative(self, item: Union[GnLabel, GnVisibility]) -> str:
+        """Returns a label relative to this target.
+
+        Args:
+            item: The GN item to rebase relative to this target.
+        """
+        return item.relative_to(self._label)
