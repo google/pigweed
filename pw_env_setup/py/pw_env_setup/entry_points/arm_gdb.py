592503204f5dd8c374599310c12c1cd44009d597
Anthony DiGirolamo <tonymd@google.com>
2023-06-15 23:13:14 +0000

    pw_env_setup: Bundle Python 3.8 with arm-gcc 12.2.1
    arm-none-eabi-gdb requires Python 3.8 to execute. This change installs
    Python 3.8 into the arm install prefix. It also adds a Python
    entrypoint script to set PYTHONHOME and PYTHONPATH to the correct
    location.
    
    Change-Id: Ia113e6fd6621629f75476389536e123271c21eae
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/151438
    Reviewed-by: Rob Mohr <mohrr@google.com>
    Commit-Queue: Anthony DiGirolamo <tonymd@google.com>

---
+++ pw_env_setup/py/pw_env_setup/entry_points/arm_gdb.py
@@ -0,0 +1,54 @@
+# Copyright 2023 The Pigweed Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Wrapper script to run arm-none-eabi-gdb with Python 3.8."""
+
+import os
+from pathlib import Path
+import shutil
+import sys
+import subprocess
+
+
+def main() -> None:
+    # Find 'arm-none-eabi-gdb' as long as it isn't in the current Python
+    # virtualenv entry point. In other words: not this script.
+    arm_gdb_binary = shutil.which(
+        'arm-none-eabi-gdb',
+        path=os.pathsep.join(
+            [
+                path_entry
+                for path_entry in os.environ.get('PATH', '').split(os.pathsep)
+                if path_entry not in sys.path
+            ]
+        ),
+    )
+    assert arm_gdb_binary
+
+    arm_gdb_path = Path(arm_gdb_binary)
+    arm_install_prefix = arm_gdb_path.parent.parent
+    python_home = arm_install_prefix / 'python/bin/python3'
+    python_path = arm_install_prefix / 'python/lib/python3.8'
+    assert arm_gdb_path.is_file()
+
+    env = os.environ.copy()
+    # Only set Python if it's in the expected location.
+    if python_home.is_file() and python_path.is_dir():
+        env['PYTHONHOME'] = str(python_home)
+        env['PYTHONPATH'] = str(python_path)
+
+    subprocess.run([str(arm_gdb_path)] + sys.argv[1:], env=env, check=False)
+
+
+if __name__ == '__main__':
+    main()
