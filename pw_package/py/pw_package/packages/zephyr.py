f4dd6053db13c09b4109e5b1cda1caffe8023779
Yuval Peress <peress@google.com>
2023-07-01 16:48:12 +0000

    zephyr: Add Zephyr SDK to the Zephyr package
    When using the Zephyr package, we want to make sure that the Zephyr
    SDK is also installed via CIPD. This was templtated from the
    arduino_core package.
    
    BUG=b/277916020
    
    Change-Id: I8a3734e522d494030816831d59795e778ff12b71
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/153870
    Reviewed-by: Anthony DiGirolamo <tonymd@google.com>
    Commit-Queue: Yuval Peress <peress@google.com>

--- pw_package/py/pw_package/packages/zephyr.py
+++ pw_package/py/pw_package/packages/zephyr.py
@@ -13,9 +13,12 @@
 # the License.
 """Install and check status of Zephyr."""
 import importlib.resources
+import json
 import pathlib
 import subprocess
 import sys
+import tempfile
+
 from typing import Sequence
 
 import pw_env_setup.virtualenv_setup
@@ -30,34 +33,83 @@ class Zephyr(pw_package.git_repo.GitRepo):
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
-            name='zephyr',
-            url='https://github.com/zephyrproject-rtos/zephyr',
-            commit='356c8cbe63ae01b3ab438382639d25bb418a0213',  # v3.4 release
+            name="zephyr",
+            url="https://github.com/zephyrproject-rtos/zephyr",
+            commit="356c8cbe63ae01b3ab438382639d25bb418a0213",  # v3.4 release
             **kwargs,
         )
 
     def info(self, path: pathlib.Path) -> Sequence[str]:
         return (
-            f'{self.name} installed in: {path}',
+            f"{self.name} installed in: {path}",
             "Enable by running 'gn args out' and adding this line:",
             f'  dir_pw_third_party_zephyr = "{path}"',
+        )
+
+    @staticmethod
+    def __populate_download_cache_from_cipd(path: pathlib.Path) -> None:
+        """Check for Zephyr SDK in cipd"""
+        package_path = path.parent.resolve()
+        core_cache_path = package_path / "zephyr_sdk"
+        core_cache_path.mkdir(parents=True, exist_ok=True)
+
+        cipd_package_subpath = "infra/3pp/tools/zephyr_sdk/${platform}"
+
+        # Check if a teensy cipd package is readable
+        with tempfile.NamedTemporaryFile(
+            prefix="cipd", delete=True
+        ) as temp_json:
+            cipd_acl_check_command = [
+                "cipd",
+                "acl-check",
+                cipd_package_subpath,
+                "-reader",
+                "-json-output",
+                temp_json.name,
+            ]
+            subprocess.run(cipd_acl_check_command, capture_output=True)
+
+            # Return if no packages are readable.
+            if not json.load(temp_json)["result"]:
+                raise RuntimeError("Failed to verify cipd is readable")
+
+        # Initialize cipd
+        subprocess.check_call(
+            [
+                "cipd",
+                "init",
+                "-force",
+                core_cache_path.as_posix(),
+            ]
+        )
+        # Install the Zephyr SDK
+        subprocess.check_call(
+            [
+                "cipd",
+                "install",
+                cipd_package_subpath,
+                "-root",
+                core_cache_path.as_posix(),
+                "-force",
+            ]
         )
 
     def install(self, path: pathlib.Path) -> None:
         super().install(path)
 
+        self.__populate_download_cache_from_cipd(path)
         with importlib.resources.path(
-            pw_env_setup.virtualenv_setup, 'constraint.list'
+            pw_env_setup.virtualenv_setup, "constraint.list"
         ) as constraint:
             subprocess.check_call(
                 [
                     sys.executable,
-                    '-m',
-                    'pip',
-                    'install',
-                    '-r',
-                    f'{path}/scripts/requirements.txt',
-                    '-c',
+                    "-m",
+                    "pip",
+                    "install",
+                    "-r",
+                    f"{path}/scripts/requirements.txt",
+                    "-c",
                     str(constraint),
                 ]
             )
