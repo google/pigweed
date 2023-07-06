99b31eb2d71ad636d1d553c0bd8efe1d9f6ab7ea
Wyatt Hepler <hepler@google.com>
2023-07-06 14:44:43 +0000

    pw_rpc: Disable flaky test until it can be fixed
    Bug: b/290048137
    Change-Id: Id28d975b7d0c7d8a466ebb7c88f34e4cf5e5611e
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/154150
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Pigweed-Auto-Submit: Wyatt Hepler <hepler@google.com>
    Reviewed-by: Rob Mohr <mohrr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>

--- pw_rpc/client_integration_test.cc
+++ pw_rpc/client_integration_test.cc
@@ -105,7 +105,9 @@ TEST(RawRpcIntegrationTest, BidirectionalStreaming) {
   }
 }
 
-TEST(RawRpcIntegrationTest, OnNextOverwritesItsOwnCall) {
+// This test sometimes fails due to a server stream packet being dropped.
+// TODO(b/290048137): Enable this test after the flakiness is fixed.
+TEST(RawRpcIntegrationTest, DISABLED_OnNextOverwritesItsOwnCall) {
   for (int i = 0; i < kIterations; ++i) {
     struct {
       StringReceiver receiver;
