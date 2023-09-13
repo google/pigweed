d2fc5bdf6c65b61ea48056b4573a8585aff40955
Rob Mohr <mohrr@google.com>
2023-09-13 16:51:55 +0000

    pw_package: Use Pigweed mirror for google/emboss
    Bug: b/278914999
    Change-Id: Ifec09617dde9305ed3b9f2b8ee33c94958f699f1
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/170356
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Pigweed-Auto-Submit: Rob Mohr <mohrr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Ted Pudlik <tpudlik@google.com>

--- pw_package/py/pw_package/packages/emboss.py
+++ pw_package/py/pw_package/packages/emboss.py
@@ -27,11 +27,9 @@ class Emboss(pw_package.git_repo.GitRepo):
         super().__init__(
             *args,
             name="emboss",
-            url="".join(
-                [
-                    "https://fuchsia.googlesource.com",
-                    "/third_party/github.com/google/emboss",
-                ]
+            url=(
+                "https://pigweed.googlesource.com"
+                "/third_party/github/google/emboss"
             ),
             commit="b02439aeecb9cc8621c7aea9fc206f65da45c9be",
             **kwargs,
