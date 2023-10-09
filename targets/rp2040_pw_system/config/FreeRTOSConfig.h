000c081d9124e161f3b1ff149a5a7985785b143f
Armando Montanez <amontanez@google.com>
2023-10-09 23:32:44 +0000

    targets/rp2040_pw_system: Enable time slicing
    Enables time slicing in FreeRTOS so more than one thread gets a chance
    to run.
    
    Change-Id: I2e38d0028c052326088ae2376bec2d78d7ddae41
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/175074
    Reviewed-by: Ted Pudlik <tpudlik@google.com>
    Pigweed-Auto-Submit: Armando Montanez <amontanez@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>

--- targets/rp2040_pw_system/config/FreeRTOSConfig.h
+++ targets/rp2040_pw_system/config/FreeRTOSConfig.h
@@ -39,7 +39,7 @@
 #define configUSE_COUNTING_SEMAPHORES           0
 #define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_QUEUE_SETS                    0
-#define configUSE_TIME_SLICING                  0
+#define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
