4079b589933785748cc47667611b03348e5689de
Faraaz Sareshwala <fsareshwala@google.com>
2023-10-30 21:28:03 +0000

    [bt][core] update comment width to 100 characters
    Change-Id: I91896e73ed254ced57604fb421927c608abd4877
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/937688
    Commit-Queue: Faraaz Sareshwala <fsareshwala@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

--- pw_bluetooth_sapphire/host/hci/low_energy_scanner.h
+++ pw_bluetooth_sapphire/host/hci/low_energy_scanner.h
@@ -25,27 +25,24 @@ struct LowEnergyScanResult {
   // The device address of the remote peer.
   DeviceAddress address;
 
-  // True if |address| is a static or random identity address resolved by the
-  // controller.
+  // True if |address| is a static or random identity address resolved by the controller.
   bool resolved = false;
 
-  // True if this peer accepts connections. This is the case if this peer
-  // sent a connectable advertising PDU.
+  // True if this peer accepts connections. This is the case if this peer sent a connectable
+  // advertising PDU.
   bool connectable = false;
 
-  // The received signal strength of the advertisement packet corresponding to
-  // this peer.
+  // The received signal strength of the advertisement packet corresponding to this peer.
   int8_t rssi = hci_spec::kRSSIInvalid;
 };
 
-// LowEnergyScanner manages Low Energy scan procedures that are used
-// during general and limited discovery and connection establishment
-// procedures. This is an abstract class that provides a common interface
-// over 5.0 Extended Advertising and Legacy Advertising features.
+// LowEnergyScanner manages Low Energy scan procedures that are used during general and limited
+// discovery and connection establishment procedures. This is an abstract class that provides a
+// common interface over 5.0 Extended Advertising and Legacy Advertising features.
 //
-// Instances of this class are expected to each as a singleton on a
-// per-transport basis as multiple instances cannot accurately reflect the state
-// of the controller while allowing simultaneous scan operations.
+// Instances of this class are expected to each as a singleton on a per-transport basis as multiple
+// instances cannot accurately reflect the state of the controller while allowing simultaneous scan
+// operations.
 class LowEnergyScanner : public LocalAddressClient {
  public:
   // Value that can be passed to StartScan() to scan indefinitely.
@@ -79,13 +76,12 @@ class LowEnergyScanner : public LocalAddressClient {
     // peer is scannable).
     virtual void OnPeerFound(const LowEnergyScanResult& result, const ByteBuffer& data);
 
-    // Called when a directed advertising report is received from the peer
-    // with the given address.
+    // Called when a directed advertising report is received from the peer with the given address.
     virtual void OnDirectedAdvertisement(const LowEnergyScanResult& result);
   };
 
   LowEnergyScanner(Transport::WeakPtr hci, pw::async::Dispatcher& pw_dispatcher);
-  virtual ~LowEnergyScanner() = default;
+  ~LowEnergyScanner() override = default;
 
   // Returns the current Scan state.
   State state() const { return state_; }
@@ -103,45 +99,41 @@ class LowEnergyScanner : public LocalAddressClient {
   // True if no scan procedure is currently enabled.
   bool IsIdle() const { return state() == State::kIdle; }
 
-  // Initiates a scan. This is an asynchronous operation that abides by
-  // the following rules:
+  // Initiates a scan. This is an asynchronous operation that abides by the following rules:
   //
-  //   - This method synchronously returns false if the procedure could not be
-  //     started, e.g. because discovery is already in progress, or it is in the
-  //     process of being stopped, or the controller does not support discovery,
-  //     etc.
+  //   - This method synchronously returns false if the procedure could not be started, e.g. because
+  //   discovery is already in progress, or it is in the process of being stopped, or the controller
+  //   does not support discovery, etc.
   //
-  //   - Synchronously returns true if the procedure was initiated but the it is
-  //     unknown whether or not the procedure has succeeded.
+  //   - Synchronously returns true if the procedure was initiated but the it is unknown whether or
+  //   not the procedure has succeeded.
   //
-  //   - |callback| is invoked asynchronously to report the status of the
-  //     procedure. In the case of failure, |callback| will be invoked once to
-  //     report the end of the procedure. In the case of success, |callback|
-  //     will be invoked twice: the first time to report that the procedure has
-  //     started, and a second time to report when the procedure ends, either
-  //     due to a timeout or cancellation.
+  //   - |callback| is invoked asynchronously to report the status of the procedure. In the case of
+  //   failure, |callback| will be invoked once to report the end of the procedure. In the case of
+  //   success, |callback| will be invoked twice: the first time to report that the procedure has
+  //   started, and a second time to report when the procedure ends, either due to a timeout or
+  //   cancellation.
   //
-  //   - |period| specifies (in milliseconds) the duration of the scan. If the
-  //     special value of kPeriodInfinite is passed then scanning will continue
-  //     indefinitely and must be explicitly stopped by calling StopScan().
-  //     Otherwise, the value must be non-zero.
+  //   - |period| specifies (in milliseconds) the duration of the scan. If the special value of
+  //   kPeriodInfinite is passed then scanning will continue indefinitely and must be explicitly
+  //   stopped by calling StopScan(). Otherwise, the value must be non-zero.
   //
-  // Once started, a scan can be terminated at any time by calling the
-  // StopScan() method. Otherwise, an ongoing scan will terminate at the end of
-  // the scan period if a finite value for |period| was provided.
+  // Once started, a scan can be terminated at any time by calling the StopScan() method. Otherwise,
+  // an ongoing scan will terminate at the end of the scan period if a finite value for |period| was
+  // provided.
   //
-  // During an active scan, scannable advertisements are reported alongside their
-  // corresponding scan response. Every scannable advertisement is stored and not
-  // reported until either
+  // During an active scan, scannable advertisements are reported alongside their corresponding scan
+  // response. Every scannable advertisement is stored and not reported until either
+  //
   //   a) a scan response is received
   //   b) an implementation determined timeout period expires
   //   c) for periodic scans, when the scan period expires
   //
-  // Since a passive scan involves no scan request/response, all advertisements are
-  // reported immediately without waiting for a scan response.
+  // Since a passive scan involves no scan request/response, all advertisements are reported
+  // immediately without waiting for a scan response.
   //
-  // (For more information about passive and active scanning, see Core Spec. v5.2,
-  // Vol 6, Part B, 4.4.3.1 and 4.4.3.2).
+  // (For more information about passive and active scanning, see Core Spec. v5.2, Vol 6, Part
+  // B, 4.4.3.1 and 4.4.3.2).
   enum class ScanStatus {
     // Reported when the scan could not be started.
     kFailed,
@@ -152,16 +144,16 @@ class LowEnergyScanner : public LocalAddressClient {
     // Reported when a passive scan was started and is currently in progress.
     kPassive,
 
-    // Called when the scan was terminated naturally at the end of the scan
-    // period.
+    // Called when the scan was terminated naturally at the end of the scan period.
     kComplete,
 
     // Called when the scan was terminated due to a call to StopScan().
     kStopped,
   };
+
   struct ScanOptions {
-    // Perform an active scan if true. During an active scan, scannable advertisements
-    // are reported alongside their corresponding scan response.
+    // Perform an active scan if true. During an active scan, scannable advertisements are reported
+    // alongside their corresponding scan response.
     bool active = false;
 
     // When enabled, the controller will filter out duplicate advertising reports. This means that
@@ -189,12 +181,12 @@ class LowEnergyScanner : public LocalAddressClient {
     uint16_t interval = hci_spec::defaults::kLEScanInterval;
     uint16_t window = hci_spec::defaults::kLEScanWindow;
   };
+
   using ScanStatusCallback = fit::function<void(ScanStatus)>;
   virtual bool StartScan(const ScanOptions& options, ScanStatusCallback callback) = 0;
 
-  // Stops a previously started scan. Returns false if a scan is not in
-  // progress. Otherwise, cancels any in progress scan procedure and returns
-  // true.
+  // Stops a previously started scan. Returns false if a scan is not in progress. Otherwise, cancels
+  // any in progress scan procedure and returns true.
   virtual bool StopScan() = 0;
 
   // Assigns the delegate for scan events.
@@ -211,8 +203,8 @@ class LowEnergyScanner : public LocalAddressClient {
 
   void set_state(State state) { state_ = state; }
 
-  // Returns true if an active scan was most recently requested. This applies to
-  // the on-going scan only if IsScanning() returns true.
+  // Returns true if an active scan was most recently requested. This applies to the on-going scan
+  // only if IsScanning() returns true.
   bool active_scan_requested() const { return active_scan_requested_; }
   void set_active_scan_requested(bool value) { active_scan_requested_ = value; }
 
