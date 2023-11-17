6d36251a6fa4ec3d71e34e2bffabf83c4ceae7fc
Ben <benlawson@google.com>
2023-11-17 17:40:44 +0000

    pw_sapphire_bluetooth: Update copyright headers
    Change-Id: I37e5594b81ddfb6c712b1b79068c9420a82b5629
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/177800
    Reviewed-by: Keir Mierle <keir@google.com>
    Reviewed-by: Josh Conner <joshconner@google.com>

--- pw_bluetooth_sapphire/public/pw_bluetooth_sapphire/internal/host/common/macros.h
+++ pw_bluetooth_sapphire/public/pw_bluetooth_sapphire/internal/host/common/macros.h
@@ -1,6 +1,16 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2023 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
 
 #ifndef SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_MACROS_H_
 #define SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_MACROS_H_
