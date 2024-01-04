fee3e609802fe3f14ae211a1055da318468882ee
Yuval Peress <peress@google.com>
2024-01-04 17:39:32 +0000

    pw_log_zephyr: Add missing include for tokenized logging
    We were missing <pw_log_tokenized/base64.h>
    
    Change-Id: Ieb14d55c241b78e78d9fb8a60ba6ed86420116d7
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/186810
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Pigweed-Auto-Submit: Yuval Peress <peress@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Armando Montanez <amontanez@google.com>

--- pw_log_zephyr/pw_log_zephyr_tokenized_handler.cc
+++ pw_log_zephyr/pw_log_zephyr_tokenized_handler.cc
@@ -12,6 +12,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+#include <pw_log_tokenized/base64.h>
 #include <pw_log_tokenized/config.h>
 #include <pw_log_tokenized/handler.h>
 #include <pw_log_tokenized/metadata.h>
