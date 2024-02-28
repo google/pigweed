6992b4612b3a18aa63ca6966cbde497972ecb23a
Christoph Klee <chklee@google.com>
2024-02-28 00:46:05 +0000

    pw_stream_uart_mcuxpresso: Implement init / deinit
    This change implements the UartStreamDmaMcuxpresso::Init
    function to initialize the USART and DMA engine.
    
    The UsartDmaTxData and UsartDmaRxData encapsulate the state
    that is required to handle a TX or RX transaction.
    
    A subsequent change will start the TX receive DMA transaction
    from this function to read into the ring buffer.
    
    Bug: 325514698
    Change-Id: I9b136e16c0cffa358a26cfc675a27e5308090743
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/192891
    Reviewed-by: Austin Foxley <afoxley@google.com>
    Commit-Queue: Christoph Klee <chklee@google.com>
    Reviewed-by: Jonathon Reinhart <jrreinhart@google.com>

--- pw_stream_uart_mcuxpresso/dma_stream.cc
+++ pw_stream_uart_mcuxpresso/dma_stream.cc
@@ -26,6 +26,29 @@
 
 namespace pw::stream {
 
+// Deinitialize the DMA channels and USART.
+void UartDmaStreamMcuxpresso::Deinit() {
+  // We need to touch register space that can be shared
+  // among several DMA peripherals, hence we need to access
+  // it exclusively. We achieve exclusive access on non-SMP systems as
+  // a side effect of acquiring the interrupt_lock_, since acquiring the
+  // interrupt_lock_ disables interrupts on the current CPU, which means
+  // we cannot get descheduled until we release the interrupt_lock_.
+  interrupt_lock_.lock();
+  DMA_DisableChannel(config_.dma_base, config_.tx_dma_ch);
+  DMA_DisableChannel(config_.dma_base, config_.rx_dma_ch);
+  interrupt_lock_.unlock();
+
+  USART_Deinit(config_.usart_base);
+}
+
+UartDmaStreamMcuxpresso::~UartDmaStreamMcuxpresso() {
+  if (!initialized_) {
+    return;
+  }
+  Deinit();
+}
+
 // Initialize the USART and DMA channels based on the configuration
 // specified during object creation.
 Status UartDmaStreamMcuxpresso::Init(uint32_t srcclk) {
@@ -41,8 +64,67 @@ Status UartDmaStreamMcuxpresso::Init(uint32_t srcclk) {
   if (config_.dma_base == nullptr) {
     return Status::InvalidArgument();
   }
+
+  usart_config_t defconfig_;
+  USART_GetDefaultConfig(&defconfig_);
+
+  defconfig_.baudRate_Bps = config_.baud_rate;
+  defconfig_.parityMode = config_.parity;
+  defconfig_.enableTx = true;
+  defconfig_.enableRx = true;
+
+  status_t status = USART_Init(config_.usart_base, &defconfig_, srcclk);
+  if (status != kStatus_Success) {
+    return Status::Internal();
+  }
+
+  // We need to touch register space that can be shared
+  // among several DMA peripherals, hence we need to access
+  // it exclusively. We achieve exclusive access on non-SMP systems as
+  // a side effect of acquiring the interrupt_lock_, since acquiring the
+  // interrupt_lock_ disables interrupts on the current CPU, which means
+  // we cannot get descheduled until we release the interrupt_lock_.
+  interrupt_lock_.lock();
+
+  INPUTMUX_Init(INPUTMUX);
+  // Enable DMA request.
+  INPUTMUX_EnableSignal(
+      INPUTMUX, config_.rx_input_mux_dmac_ch_request_en, true);
+  INPUTMUX_EnableSignal(
+      INPUTMUX, config_.tx_input_mux_dmac_ch_request_en, true);
+  // Turnoff clock to inputmux to save power. Clock is only needed to make
+  // changes.
+  INPUTMUX_Deinit(INPUTMUX);
+
+  DMA_EnableChannel(config_.dma_base, config_.tx_dma_ch);
+  DMA_EnableChannel(config_.dma_base, config_.rx_dma_ch);
+
+  DMA_CreateHandle(&tx_data_.dma_handle, config_.dma_base, config_.tx_dma_ch);
+  DMA_CreateHandle(&rx_data_.dma_handle, config_.dma_base, config_.rx_dma_ch);
+
+  interrupt_lock_.unlock();
+
+  status = USART_TransferCreateHandleDMA(config_.usart_base,
+                                         &uart_dma_handle_,
+                                         TxRxCompletionCallback,
+                                         this,
+                                         &tx_data_.dma_handle,
+                                         &rx_data_.dma_handle);
+
+  if (status != kStatus_Success) {
+    Deinit();
+    return Status::Internal();
+  }
+
+  initialized_ = true;
   return OkStatus();
 }
+
+// Completion callback for TX and RX transactions
+void UartDmaStreamMcuxpresso::TxRxCompletionCallback(USART_Type* base,
+                                                     usart_dma_handle_t* state,
+                                                     status_t status,
+                                                     void* param) {}
 
 StatusWithSize UartDmaStreamMcuxpresso::DoRead(ByteSpan data) {
   size_t length = data.size();
--- pw_stream_uart_mcuxpresso/public/pw_stream_uart_mcuxpresso/dma_stream.h
+++ pw_stream_uart_mcuxpresso/public/pw_stream_uart_mcuxpresso/dma_stream.h
@@ -13,12 +13,16 @@
 // the License.
 #pragma once
 
+#include <atomic>
+
 #include "fsl_dma.h"
 #include "fsl_inputmux.h"
 #include "fsl_usart_dma.h"
 #include "pw_bytes/span.h"
 #include "pw_status/status.h"
 #include "pw_stream/stream.h"
+#include "pw_sync/interrupt_spin_lock.h"
+#include "pw_sync/thread_notification.h"
 
 namespace pw::stream {
 
@@ -38,17 +42,68 @@ class UartDmaStreamMcuxpresso final : public NonSeekableReaderWriter {
     ByteSpan buffer;                                    // Receive ring buffer
   };
 
-  UartDmaStreamMcuxpresso(const Config& config) : config_(config) {}
+  UartDmaStreamMcuxpresso(const Config& config)
+      : config_(config), rx_data_{.ring_buffer = config.buffer} {}
+
+  ~UartDmaStreamMcuxpresso();
+
+  UartDmaStreamMcuxpresso(const UartDmaStreamMcuxpresso& other) = delete;
+
+  UartDmaStreamMcuxpresso& operator=(const UartDmaStreamMcuxpresso& other) =
+      delete;
 
   pw::Status Init(uint32_t srcclk);
 
  private:
+  // Usart DMA TX data structure
+  struct UsartDmaTxData {
+    ConstByteSpan buffer;       // TX transaction buffer
+    size_t tx_idx;              // Position within TX transaction
+    dma_handle_t dma_handle;    // DMA handle
+    usart_transfer_t transfer;  // USART TX transfer structure
+    std::atomic_uint8_t busy;   // Flag to prevent concurrent access to TX queue
+    pw::sync::ThreadNotification notification;  // TX completion notification
+  };
+
+  // Usart DMA RX data structure
+  struct UsartDmaRxData {
+    ByteSpan ring_buffer;          // Receive ring buffer
+    size_t ring_buffer_read_idx;   // ring buffer reader index
+    size_t ring_buffer_write_idx;  // ring buffer writer index
+    size_t
+        data_received;  // data received and acknowledged by completion callback
+    size_t data_copied;  // data copied out to receiver
+    // completion callback will be executed when completion size decreases to 0
+    // bytes
+    size_t completion_size;
+    usart_transfer_t transfer;  // USART RX transfer structure
+    dma_handle_t dma_handle;    // DMA handle
+    std::atomic_uint8_t
+        busy;  // Flag to prevent concurrent access to RX ring buffer
+    pw::sync::ThreadNotification notification;  // RX completion notification
+  };
+
   StatusWithSize DoRead(ByteSpan) override;
   Status DoWrite(ConstByteSpan) override;
 
+  // Helper functions
+  static IRQn_Type GetInterrupt(const DMA_Type* base);
+  void Deinit();
+  static void TxRxCompletionCallback(USART_Type* base,
+                                     usart_dma_handle_t* state,
+                                     status_t status,
+                                     void* param);
+
+  pw::sync::InterruptSpinLock
+      interrupt_lock_;  // Lock to synchronize with interrupt handler and to
+                        // guarantee exclusive access to DMA control registers
   usart_dma_handle_t uart_dma_handle_;  // USART DMA Handle
+  struct UsartDmaTxData tx_data_;       // TX data
+  struct UsartDmaRxData rx_data_;       // RX data
 
   Config const config_;  // USART DMA configuration
+  bool
+      initialized_;  // Whether the USART and DMA channels have been initialized
 };
 
 }  // namespace pw::stream
