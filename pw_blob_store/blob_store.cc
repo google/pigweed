8dcf8a56347094507ab0b24dd10d3227c481308f
David Rogers <davidrogers@google.com>
2024-03-07 18:20:37 +0000

    pw_blob_store: Set module name to BLOB
    Set PW_LOG_MODULE_NAME for blob_store module to BLOB
    
    Change-Id: I8b8eba5847d50f3c4d508ddb1f8ff44728fe0705
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/195170
    Reviewed-by: Armando Montanez <amontanez@google.com>
    Pigweed-Auto-Submit: David Rogers <davidrogers@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>

--- pw_blob_store/blob_store.cc
+++ pw_blob_store/blob_store.cc
@@ -12,6 +12,8 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+#define PW_LOG_MODULE_NAME "BLOB"
+
 #include "pw_blob_store/blob_store.h"
 
 #include <algorithm>
