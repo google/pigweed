df32be4ec5003b9c27fd67a1e80d21013ad5b462
Erik Gilling <konkers@google.com>
2024-03-08 02:07:27 +0000

    pw_tokenizer: Refactor Rust hash code to allow multi-input hashing
    Change-Id: I6c4641c8e96a75a3adfc0503dd721f6bffd19da8
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/186310
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Pigweed-Auto-Submit: Erik Gilling <konkers@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Taylor Cramer <cramertj@google.com>

--- pw_tokenizer/rust/pw_tokenizer_core.rs
+++ pw_tokenizer/rust/pw_tokenizer_core.rs
@@ -22,6 +22,69 @@
 
 pub const HASH_CONSTANT: u32 = 65599u32;
 
+/// Hasher is used to calculate a token's hash value.
+///
+/// The hash state is encapsulated in the `Hasher` to allow the hashing of
+/// multi-part strings where the concatenated value can not be know at macro
+/// expansion time.  All functions are `const`.
+pub struct Hasher {
+    coefficient: u32,
+    hash: u32,
+    bytes_hashed: usize,
+    hash_len: usize,
+}
+
+impl Hasher {
+    /// Create a new `Hasher`
+    ///
+    /// `data_len` is the total length of the data to be hashed.  `hash_len`
+    /// is the number of bytes of data to be used in calculating the hash.
+    /// `data_len` is used to seed  the hash while `hash_len` controls how many
+    /// bytes are hashed.
+    pub const fn new(data_len: usize, hash_len: usize) -> Self {
+        {
+            Self {
+                coefficient: HASH_CONSTANT,
+                hash: data_len as u32,
+                bytes_hashed: 0,
+                hash_len,
+            }
+        }
+    }
+
+    /// Processes `bytes` and updates hash state.
+    ///
+    /// Consumes `self` and returns a [`Hasher`] with the updated state.
+    pub const fn process_bytes(mut self, bytes: &[u8]) -> Self {
+        let bytes_left = self.hash_len - self.bytes_hashed;
+
+        let bytes = if bytes.len() > bytes_left {
+            bytes.split_at(bytes_left).0
+        } else {
+            bytes
+        };
+
+        // For loops are not allowed in const functions.
+        let mut i = 0;
+        while i < bytes.len() {
+            // We call `u32::wrapping_*` instead of using `core::num::Wrapping` to
+            // avoid using traits in a const function.
+            self.hash = self
+                .hash
+                .wrapping_add(self.coefficient.wrapping_mul(bytes[i] as u32));
+            self.coefficient = self.coefficient.wrapping_mul(HASH_CONSTANT);
+            i += 1;
+        }
+        self.bytes_hashed += i;
+
+        self
+    }
+
+    /// Consume `self` and return the hash.
+    pub const fn hash(self) -> u32 {
+        self.hash
+    }
+}
 /// Calculate the hash for a sequence of bytes.
 ///
 /// ```
@@ -43,29 +106,7 @@ pub const fn hash_bytes(bytes: &[u8]) -> u32 {
 /// assert_eq!(hash, 0x92c5d2ac);
 /// ```
 pub const fn hash_bytes_fixed(bytes: &[u8], len: usize) -> u32 {
-    // The length is hashed as if it were the first byte.
-    let mut hash = bytes.len() as u32;
-    let mut coefficient = HASH_CONSTANT;
-
-    // Fixed sized hashes are seeded with the total length of the slice
-    // but only hash over at most `len` bytes.
-    let bytes = if bytes.len() > len {
-        bytes.split_at(len).0
-    } else {
-        bytes
-    };
-
-    // For loops are not allowed in const functions.
-    let mut i = 0;
-    while i < bytes.len() {
-        // We call `u32::wrapping_*` instead of using `core::num::Wrapping` to
-        // avoid using traits in a const function.
-        hash = hash.wrapping_add(coefficient.wrapping_mul(bytes[i] as u32));
-        coefficient = coefficient.wrapping_mul(HASH_CONSTANT);
-        i += 1;
-    }
-
-    hash
+    Hasher::new(bytes.len(), len).process_bytes(bytes).hash()
 }
 
 /// Calculate the hash for a string.
@@ -84,7 +125,7 @@ pub const TOKENIZER_ENTRY_MAGIC: u32 = 0xBAA98DEE;
 
 #[cfg(test)]
 mod tests {
-    use super::hash_bytes_fixed;
+    use super::{hash_bytes_fixed, Hasher};
 
     struct TestCase {
         string: &'static [u8],
@@ -96,9 +137,27 @@ mod tests {
     include!("pw_tokenizer_core_test_cases.rs");
 
     #[test]
-    fn hash() {
+    fn hash_bytes_fixed_generates_correct_hash() {
         for test in test_cases() {
             let hash = hash_bytes_fixed(test.string, test.hash_length);
+            assert_eq!(
+                hash, test.hash,
+                "{:08x} != {:08x} string: {:x?} hash_size: {}",
+                hash, test.hash, test.string, test.hash_length
+            );
+        }
+    }
+
+    #[test]
+    fn multi_part_data_generates_correct_hash() {
+        for test in test_cases() {
+            let mut hasher = Hasher::new(test.string.len(), test.hash_length);
+            // Break each test string into 8 byte chunks and feed them to the
+            // hasher separately.
+            for chunk in test.string.chunks(8) {
+                hasher = hasher.process_bytes(chunk);
+            }
+            let hash = hasher.hash();
             assert_eq!(
                 hash, test.hash,
                 "{:08x} != {:08x} string: {:x?} hash_size: {}",
